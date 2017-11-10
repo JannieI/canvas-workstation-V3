5fbd9a8822eb905127db3560af4f439b3d80ded9
JannieI <jimmelman@gmail.com>
2017-11-10 21:09:00 +0200

    Correct skeleton sub-menu for Dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -218,3 +218,17 @@ export class DashboardComponent {
         console.log(dl.mutual.dist(counts, 'bin_price', 'year_date', 'count'));
     }
 }
+
+
+
+// /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+// }
+
+// /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+// }
