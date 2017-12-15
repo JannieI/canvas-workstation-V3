5a523e4b7adafa7bd3d215c9ce0598f7c42c176f
JannieI <jimmelman@gmail.com>
2017-12-15 09:24:46 +0200

    Add ngIf and code for different types of shapes

--- src/app/shape.edit.component.ts
+++ src/app/shape.edit.component.ts
@@ -23,7 +23,13 @@ export class ShapeEditComponent implements OnInit {
     @Input() currentWidgetSpec: any;
     @Output() formShapeEditClosed: EventEmitter<string> = new EventEmitter();
 
+    showLine: boolean = false;  
+    showCircle: boolean = false;  
+    showText: boolean = true;  
+    showImage: boolean = false;  
     showTypeDashboard: boolean = false;  
+    showRectangle: boolean = false;  
+    showArrow: boolean = false;  
 
     ngOnInit() {
 
@@ -33,5 +39,60 @@ export class ShapeEditComponent implements OnInit {
         console.log('clickClose')
         
 		this.formShapeEditClosed.emit(action);
+    }
+
+    clickSelectShape(ev) {
+        console.log(ev, ev.srcElement.value)
+        let shape: string = ev.srcElement.value;
+
+        if (shape == 'TextBox') {
+            this.showLine = false;
+            this.showCircle = false;
+            this.showText = true;
+            this.showImage = false;
+            this.showRectangle = false;
+            this.showArrow = false;
+        }
+        if (shape == 'Circle') {
+            this.showLine = false;
+            this.showCircle = true;
+            this.showText = false;
+            this.showImage = false;
+            this.showRectangle = false;
+            this.showArrow = false;
+        }
+        if (shape == 'Rectangle') {
+            this.showLine = false;
+            this.showCircle = false;
+            this.showText = false;
+            this.showImage = false;
+            this.showRectangle = true;
+            this.showArrow = false;
+        }
+        if (shape == 'Line') {
+            this.showLine = true;
+            this.showCircle = false;
+            this.showText = false;
+            this.showImage = false;
+            this.showRectangle = false;
+            this.showArrow = false;
+        }
+        if (shape == 'Arrow') {
+            this.showLine = false;
+            this.showCircle = false;
+            this.showText = false;
+            this.showImage = false;
+            this.showRectangle = false;
+            this.showArrow = true;
+        }
+        if (shape == 'Image') {
+            this.showLine = false;
+            this.showCircle = false;
+            this.showText = false;
+            this.showImage = false;
+            this.showRectangle = false;
+            this.showArrow = false;
+        }
+
     }
 }
