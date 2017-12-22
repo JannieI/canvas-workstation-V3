7b39fca9c9371d2c6c4a41ea042e62d31567687e
JannieI <jimmelman@gmail.com>
2017-12-22 12:48:15 +0200

    Message form, v1

---
+++ src/app/collaborate.message.ts
@@ -0,0 +1,53 @@
+/*
+ * Visualise page, to view / present Dashboards previously created
+ */
+
+// Angular
+import { Component }                  from '@angular/core';
+import { EventEmitter }               from '@angular/core';
+import { Input }                      from '@angular/core';
+import { OnInit }                     from '@angular/core';
+import { Output }                     from '@angular/core';
+import { Router }                     from '@angular/router';
+
+// Our Functions
+import { GlobalFunctionService } 	  from './global-function.service';
+
+// Our Services
+import { GlobalVariableService}       from './global-variable.service';
+
+// Models
+import { dashboard }                  from './models';
+import { canvasMessage }              from './models';
+
+@Component({
+    selector: 'collaborate-messages',
+    templateUrl: './collaborate.messages.component.html',
+    styleUrls: ['./collaborate.messages.component.css']
+})
+export class CollaborateMessagesComponent implements OnInit {
+
+    @Input() currentWidgetSpec: any;
+    @Output() formCollaborateMessagesClosed: EventEmitter<string> = new EventEmitter();
+
+    showTypeDashboard: boolean = false;
+    dashboards: dashboard[];
+    canvasMessages: canvasAlert[];
+   
+
+	constructor(
+        private globalFunctionService: GlobalFunctionService,
+        private globalVariableService: GlobalVariableService,
+	) {}
+
+    ngOnInit() {
+        this.dashboards = this.globalVariableService.dashboards;
+        this.canvasMessages = this.globalVariableService.canvasMessages;
+    }
+
+    clickClose(action: string) {
+        console.log('clickClose')
+
+		this.formCollaborateMessagesClosed.emit(action);
+    }
+}
