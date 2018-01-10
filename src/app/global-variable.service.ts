7bcf3c89de810b2f1a48306bb4d23a5833ac38f0
JannieI <jimmelman@gmail.com>
2018-01-10 07:45:10 +0200

    Improve shape layout

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -1537,7 +1537,7 @@ export class GlobalVariableService {
     showMainMenu = new BehaviorSubject<boolean>(true);
     // Opening forms
     openDashboardFormOnStartup: boolean = false;
-    showModalLanding: boolean = true;  // Shows Landing page
+    showModalLanding: boolean = false;  // Shows Landing page
     // Session
     currentDashboardID = new BehaviorSubject<number>(null);
     sessionDebugging: boolean = true;
