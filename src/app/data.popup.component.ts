ebc90da52d537a3fda0bd43d9a7149fefee7bd28
JannieI <jimmelman@gmail.com>
2018-02-21 18:40:36 +0200

    Neaten data form

--- src/app/data.popup.component.ts
+++ src/app/data.popup.component.ts
@@ -186,11 +186,15 @@ export class DataPopupComponent implements OnInit {
 
     clickDSPreview(folderName: string, fileName: string) {
         // Load the new DS in the ID section, and show in Preview area
-        this.globalFunctionService.printToConsole(this.constructor.name,'clickDSPreview', '@Start');
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickDSPreview',           '@Start');
 
+        // Reset
         this.errorMessage = '';
         this.fileName = fileName;
+        this.showDataPreview = false;
         console.log('xx brws', folderName + this.fileName)
+
+        // Get the folder and file, setting some defaults
         if (folderName == ''  ||  folderName == undefined) {
             folderName = './assets/vega-datasets/';
         }
@@ -210,7 +214,7 @@ export class DataPopupComponent implements OnInit {
         if (fileSuffix == 'json') {
             dl.json({url: filePath}, {}, (err, currentData) => {
                 if (err) {
-                    this.errorMessage = err;
+                    this.errorMessage = err.status + ':' + err.statusText;
                     console.log('DataPopup clickDSPreview error on load', err)
                 } else {
                     // Callback
@@ -221,11 +225,13 @@ export class DataPopupComponent implements OnInit {
         if (fileSuffix == 'csv') {
             dl.csv({url: filePath}, {}, (err, currentData) => {
                 if (err) {
-                    this.errorMessage = err;
+                    this.errorMessage = err.status + ':' + err.statusText;
                     console.log('DataPopup clickDSPreview error on load', err)
                 } else {
                     // Callback
                     this.fileLoadedCallback(fileSuffix, currentData);
+
+                    this.showDataPreview = true;
                 }
             });
         };
@@ -244,116 +250,117 @@ export class DataPopupComponent implements OnInit {
 
         startNow = Date.now()
 
-                // Load
-                console.log('')
-                console.log('DataPopup clickDSPreview LOAD start:')
-                this.currentData = currentData;
-                this.globalVariableService.datasets.push(
-                    {
-                        datasourceID : 3,
-                        data: currentData
-                    }
-                );
-                currentData = [];
-                console.log('DataPopup clickDSPreview      data rows', this.currentData.length)
-                console.log('DataPopup clickDSPreview      END load: ', (Date.now() - startNow) / 1000)
+        // Load
+        console.log('')
+        console.log('DataPopup clickDSPreview LOAD start:')
+        this.currentData = currentData;
+        this.globalVariableService.datasets.push(
+            {
+                datasourceID : 3,
+                data: currentData
+            }
+        );
+        currentData = [];
+        console.log('DataPopup clickDSPreview      data rows', this.currentData.length)
+        console.log('DataPopup clickDSPreview      END load: ', (Date.now() - startNow) / 1000)
 
-                // Fields
-                console.log('')
-                console.log('DataPopup clickDSPreview FIELDS start:')
-                startNow = Date.now()
-                var dataTypes = dl.type.all(this.currentData)
-                this.dataFieldNames = Object.keys(dataTypes);
-                console.log('DataPopup clickDSPreview      fields', this.dataFieldNames)
-                for (var i = 0; i < this.dataFieldNames.length; i++) {
-                    console.log('     ', i, this.dataFieldNames[i])
-                }
-                console.log('DataPopup clickDSPreview      END fields: ', (Date.now() - startNow) / 1000)
+        // Fields
+        console.log('')
+        console.log('DataPopup clickDSPreview FIELDS start:')
+        startNow = Date.now()
+        var dataTypes = dl.type.all(this.currentData)
+        this.dataFieldNames = Object.keys(dataTypes);
+        console.log('DataPopup clickDSPreview      fields', this.dataFieldNames)
+        for (var i = 0; i < this.dataFieldNames.length; i++) {
+            console.log('     ', i, this.dataFieldNames[i])
+        }
+        console.log('DataPopup clickDSPreview      END fields: ', (Date.now() - startNow) / 1000)
 
-                // Types
-                console.log('')
-                console.log('DataPopup clickDSPreview TYPES start:')
-                startNow = Date.now()
-                this.dataFieldTypes = [];
-                console.log('DataPopup clickDSPreview      types');
-                for (var i = 0; i < this.dataFieldNames.length; i++) {
-                    this.dataFieldTypes.push(dataTypes[ this.dataFieldNames[i] ] );
-                    console.log('DataPopup clickDSPreview      ', i, this.dataFieldTypes[i])
-                }
-                console.log('DataPopup clickDSPreview      END types: ', (Date.now() - startNow) / 1000)
+        // Types
+        console.log('')
+        console.log('DataPopup clickDSPreview TYPES start:')
+        startNow = Date.now()
+        this.dataFieldTypes = [];
+        console.log('DataPopup clickDSPreview      types');
+        for (var i = 0; i < this.dataFieldNames.length; i++) {
+            this.dataFieldTypes.push(dataTypes[ this.dataFieldNames[i] ] );
+            console.log('DataPopup clickDSPreview      ', i, this.dataFieldTypes[i])
+        }
+        console.log('DataPopup clickDSPreview      END types: ', (Date.now() - startNow) / 1000)
 
-                // Lengths
-                console.log('')
-                console.log('DataPopup clickDSPreview LENGTHS start:')
-                startNow = Date.now()
-                this.dataFieldLengths = [];
-                console.log('DataPopup clickDSPreview      lengths');
-                for (var i = 0; i < this.dataFieldTypes.length; i++) {
-                    if (this.dataFieldTypes[i] == 'string'  ||  this.dataFieldTypes[i] == 'date') {
-                        this.dataFieldLengths.push(25);
-                    } else {
-                        this.dataFieldLengths.push(12);
-                    }
-                    console.log('DataPopup clickDSPreview      ', i, this.dataFieldLengths[i])
-                }
-                console.log('DataPopup clickDSPreview      END lengths: ', (Date.now() - startNow) / 1000)
+        // Lengths
+        console.log('')
+        console.log('DataPopup clickDSPreview LENGTHS start:')
+        startNow = Date.now()
+        this.dataFieldLengths = [];
+        console.log('DataPopup clickDSPreview      lengths');
+        for (var i = 0; i < this.dataFieldTypes.length; i++) {
+            if (this.dataFieldTypes[i] == 'string'  ||  this.dataFieldTypes[i] == 'date') {
+                this.dataFieldLengths.push(25);
+            } else {
+                this.dataFieldLengths.push(12);
+            }
+            console.log('DataPopup clickDSPreview      ', i, this.dataFieldLengths[i])
+        }
+        console.log('DataPopup clickDSPreview      END lengths: ', (Date.now() - startNow) / 1000)
 
-                // Sort
-                console.log('')
-                console.log('DataPopup clickDSPreview SORT start:')
-                startNow = Date.now()
-                this.currentData.sort(dl.comparator(['+symbol', '-price']));
-                console.log('DataPopup clickDSPreview      END sort: ', (Date.now() - startNow) / 1000)
+        // Sort
+        console.log('')
+        console.log('DataPopup clickDSPreview SORT start:')
+        startNow = Date.now()
+        this.currentData.sort(dl.comparator(['+symbol', '-price']));
+        console.log('DataPopup clickDSPreview      END sort: ', (Date.now() - startNow) / 1000)
 
-                // Group By
-                console.log('')
-                console.log('DataPopup clickDSPreview GROUPBY start:')
-                startNow = Date.now()
-                this.dataArray = dl.groupby('symbol')
-                    .summarize( [
-                        {name: 'symbol', ops: ['valid']},
-                        {name: 'price',  ops: ['sum', 'median'], as: ['s', 'm']}
-                        ] )
-                    .execute(this.currentData);
-                console.log('DataPopup clickDSPreview      groupby', this.dataArray)
-                console.log('DataPopup clickDSPreview      END groupby: ', (Date.now() - startNow) / 1000)
+        // Group By
+        console.log('')
+        console.log('DataPopup clickDSPreview GROUPBY start:')
+        startNow = Date.now()
+        this.dataArray = dl.groupby('symbol')
+            .summarize( [
+                {name: 'symbol', ops: ['valid']},
+                {name: 'price',  ops: ['sum', 'median'], as: ['s', 'm']}
+                ] )
+            .execute(this.currentData);
+        console.log('DataPopup clickDSPreview      groupby', this.dataArray)
+        console.log('DataPopup clickDSPreview      END groupby: ', (Date.now() - startNow) / 1000)
 
-                // Get Unique Symbols
-                console.log('')
-                console.log('DataPopup clickDSPreview UNIQUE start:')
-                startNow = Date.now()
-                var dataUniqueInColumn = dl.unique(this.currentData);
-                console.log('DataPopup clickDSPreview      unique', dataUniqueInColumn)
-                console.log('DataPopup clickDSPreview      END unique: ', (Date.now() - startNow) / 1000)
+        // Get Unique Symbols
+        console.log('')
+        console.log('DataPopup clickDSPreview UNIQUE start:')
+        startNow = Date.now()
+        var dataUniqueInColumn = dl.unique(this.currentData);
+        console.log('DataPopup clickDSPreview      unique', dataUniqueInColumn)
+        console.log('DataPopup clickDSPreview      END unique: ', (Date.now() - startNow) / 1000)
 
-                // Get Unique Symbols 2
-                console.log('')
-                console.log('DataPopup clickDSPreview UNIQUE 2 start:')
-                startNow = Date.now()
-                dataUniqueInColumn = dl.groupby('symbol')
-                    .summarize( [
-                        {name: 'symbol', ops: ['values']}
-                        ] )
-                    .execute(this.currentData);
-                console.log('DataPopup clickDSPreview      unique', dataUniqueInColumn)
-                console.log('DataPopup clickDSPreview      END unique: ', (Date.now() - startNow) / 1000)
+        // Get Unique Symbols 2
+        console.log('')
+        console.log('DataPopup clickDSPreview UNIQUE 2 start:')
+        startNow = Date.now()
+        dataUniqueInColumn = dl.groupby('symbol')
+            .summarize( [
+                {name: 'symbol', ops: ['values']}
+                ] )
+            .execute(this.currentData);
+        console.log('DataPopup clickDSPreview      unique', dataUniqueInColumn)
+        console.log('DataPopup clickDSPreview      END unique: ', (Date.now() - startNow) / 1000)
 
-                // Preview
-                console.log('')
-                console.log('DataPopup clickDSPreview PREVIEW start:')
-                startNow = Date.now()
-                console.log('DataPopup clickDSPreview         END preview: ', (Date.now() - startNow) / 1000)
+        // Preview
+        console.log('')
+        console.log('DataPopup clickDSPreview PREVIEW start:')
+        startNow = Date.now()
+        console.log('DataPopup clickDSPreview         END preview: ', (Date.now() - startNow) / 1000)
 
-                // No DS currently selected
-                this.currentDatasetName = '';
+        // No DS currently selected
+        this.currentDatasetName = '';
 
-                // Show the Preview button
-                this.showDataPreview = true; //!this.showDataPreview;
+        // Show the Preview button
+        this.showDataPreview = true; 
 
-                // Show Add button
-                this.showAddButton = true;
-        
+        // Show Add button
+        this.showAddButton = true;
+
     }
+
     clickDSAdd(action: string) {
         //
         this.globalFunctionService.printToConsole(this.constructor.name,'clickDSAdd', '@Start');
