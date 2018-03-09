6e48d9b6ba3f49c4a70f06b3770b4c6f7ef7cb0e
JannieI <jimmelman@gmail.com>
2018-03-09 16:50:57 +0200

    Depricate showWidgetForSlicer

--- src/app/widget.single.component.ts
+++ src/app/widget.single.component.ts
@@ -229,24 +229,6 @@ export class WidgetSingleComponent {
         console.log('TEST refreshWidget end')
     }
 
-    showWidgetForSlicer(id: number, datasourceID: number, datasetID: number) {
-        // Returns True if a Widget is related to the selected Sl(s)
-        // TODO - put back, but this fires ALL the time ...
-        // this.globalFunctionService.printToConsole(this.constructor.name,'showWidgetForSlicer', '@Start');
-
-        // Get list of selected Sl
-        let result: boolean = false;
-        this.globalVariableService.currentWidgets.forEach(sl => {
-            if (sl.isSelected  &&  sl.widgetType == 'Slicer') {
-                if (sl.datasourceID == datasourceID  &&  sl.datasetID == datasetID) {
-                    result = true;
-                };
-            };
-        });
-
-        return result;
-    }
-
     clickSingleWidget(index: number, id: number) {
         // Click W object
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSingleWidget', '@Start');
