40f2d29dda1e1b0f6b323261fe02a1d11360bd44
JannieI <jimmelman@gmail.com>
2018-03-15 19:23:18 +0200

    Clean code in StatusBar

--- src/app/statusbar.component.ts
+++ src/app/statusbar.component.ts
@@ -5,19 +5,9 @@
 // From Angular
 import { AfterViewInit }              from '@angular/core';
 import { Component }                  from '@angular/core';
-import { Directive }                  from '@angular/core';
-import { ElementRef }                 from '@angular/core';
-import { HostBinding }                from '@angular/core';
-import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
-import { QueryList }                  from '@angular/core';
-import { Renderer }                   from '@angular/core';
-import { Renderer2 }                  from '@angular/core';
-import { ViewChild }                  from '@angular/core';
-import { ViewChildren }               from '@angular/core';
 
-import { Observable} from 'rxjs'
 // Our Services
 import { GlobalVariableService }      from './global-variable.service';
 import { GlobalFunctionService }      from './global-function.service';
@@ -27,89 +17,6 @@ import { Dashboard }                  from './models'
 import { DashboardTab }               from './models'
 import { Datasource }                 from './models'
 
-// Vega, Vega-Lite
-import { compile }                    from 'vega-lite';
-import { parse }                      from 'vega';
-import { View }                       from 'vega';
-import * as dl from 'datalib';
-import { load } from 'datalib';
-import { BoxPlotStyle } from 'vega-lite/build/src/compositemark/boxplot';
-
-// Own Services
-
-// Own Components
-
-// Constants
-const vlTemplate: dl.spec.TopLevelExtendedSpec =
-{
-    "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
-
-    // Properties for top-level specification (e.g., standalone single view specifications)
-    "background": "",
-    "padding": "",
-    // "autosize": "",          NB - add these only if needed, blank causes no graph display
-    // "config": "",            NB - add these only if needed, blank causes no graph display
-
-    // Properties for any specifications
-    "title":
-        {
-            "text": "",
-            "anchor": "",
-            "offset": "",
-            "orient": "",
-            "style": ""
-        },
-    "name": "",
-    "transform": "",
-
-    "description": "",
-    "data": "",
-    "mark":
-        {
-            "type": "",  //bar circle square tick line area point rule text
-            "style": "",
-            "clip": ""
-        },
-    "encoding":
-        {
-            "x":
-                {
-                    "aggregate": "",
-                    "field": "",
-                    "type": "ordinal",
-                    "bin": "",
-                    "timeUnit": "",
-                    "axis":
-                    {
-                        "title": ""
-                    },
-                    "scale": "",
-                    "legend": "",
-                    "format": "",
-                    "stack": "",
-                    "sort": "",
-                    "condition": ""
-                },
-            "y":
-                {
-                    "aggregate": "",
-                    "field": "",
-                    "type": "quantitative",
-                    "bin": "",
-                    "timeUnit": "",
-                    "axis":
-                        {
-                            "title": ""
-                        },
-                    "scale": "",
-                    "legend": "",
-                    "format": "",
-                    "stack": "",
-                    "sort": "",
-                    "condition": ""
-                    }
-        }
-};
 
 @Component({
     selector: 'status-bar',
@@ -146,8 +53,6 @@ export class StatusbarComponent {
         // private globalFunctionService: GlobalFunctionService,
         private globalFunctionService: GlobalFunctionService,
         private globalVariableService: GlobalVariableService,
-        private renderer: Renderer,
-        private renderer2: Renderer2,
     ) {}
 
     ngOnInit() {
@@ -355,11 +260,42 @@ export class StatusbarComponent {
             return;
         };
 
+        this.globalVariableService.dashboardTabs.splice(x, 1)
+        this.globalVariableService.refreshCurrentDashboard(
+            'statusbar-clickShowLastTab',
+            this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
+            0,
+            'Previous'
+        );
         this.showNewTab = true;
     }
 
+    clickTabDelete() {
+        // Delete a Tab
+        this.globalFunctionService.printToConsole(this.constructor.name,'handleCloseDashboardTab', '@Start');
+
+        let nrWperT: number = 0;
+        nrWperT = this.globalVariableService.currentWidgets.filter(w => {
+            w.dashboardTabIDs.indexOf(this.globalVariableService.currentDashboardInfo.value.currentDashboardTabID)
+        }).length;
+        if (nrWperT > 0) {
+            this.globalVariableService.statusBarMessage.next(
+                {
+                    message: 'First delete all Widgets',
+                    uiArea: 'StatusBar',
+                    classfication: 'Warning',
+                    timeout: 3000,
+                    defaultMessage: ''
+                }
+            );
+            return;
+        };
+
+        this.showDashboardTabDescription = false;
+    }
+
     handleCloseDashboardTab(action: string) {
-        //
+        // Handle Close Tab form
         this.globalFunctionService.printToConsole(this.constructor.name,'handleCloseDashboardTab', '@Start');
 
         this.showNewTab = false;
