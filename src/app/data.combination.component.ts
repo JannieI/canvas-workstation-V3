fa5a87439872d37b5d9ccca79621d92ce88a7491
JannieI <jimmelman@gmail.com>
2018-03-18 09:24:53 +0200

    Update Data Combination

--- src/app/data.combination.component.ts
+++ src/app/data.combination.component.ts
@@ -50,6 +50,15 @@ export class DataCombinationComponent implements OnInit {
         )
     }
 
+    ngOnDestroy() {
+        // Cleanup just before Angular destroys the directive/component. 
+        // Unsubscribe Observables and detach event handlers to avoid memory leaks.
+        // Called just before Angular destroys the directive/component.
+        this.globalFunctionService.printToConsole(this.constructor.name,'ngOnDestroy', '@Start');
+
+        this.globalVariableService.isFirstTimeDataCombination.unsubscribe();
+    }
+
     clickClose(action: string) {
         console.log('clickClose')
 
