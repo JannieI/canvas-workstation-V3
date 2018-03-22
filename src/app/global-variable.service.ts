9282ebcdb81ce93b942a4b8196d0fee95bc051ae
JannieI <jimmelman@gmail.com>
2018-03-22 10:14:25 +0200

    Add saveUserPaletteButtonBar

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -2741,6 +2741,33 @@ export class GlobalVariableService {
 
     }
 
+    saveUserPaletteButtonBar(data: UserPaletteButtonBar): Promise<string> {
+        // Description: Saves UserPaletteButtonBar
+        // Returns: 'Saved' or error message
+        console.log('Global-Variables saveUserPaletteButtonBar ...');
+
+        let url: string = 'UserPaletteButtonBars';
+        this.filePath = './assets/data.UserPaletteButtonBars.json';
+
+        return new Promise<string>((resolve, reject) => {
+
+            const headers = new HttpHeaders()
+                .set("Content-Type", "application/json");
+
+            this.http.put('http://localhost:3000/' + url + '/' + data.id, data, {headers})
+            .subscribe(
+                data => {
+                    console.log('xx saveUserPaletteButtonBar SAVED', data)
+                    resolve('Saved');
+                },
+                err => {
+                    console.log('xx saveUserPaletteButtonBar FAILED', err);;
+                    resolve(err.Message);
+                }
+            )
+        });
+    }
+
     getWidgets(): Promise<Widget[]> {
         // Description: Gets all W
         // Returns: this.widgets array, unless:
