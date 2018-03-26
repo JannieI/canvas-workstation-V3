865a4142894f6876e9fc1ffb299dcb84bca83b78
JannieI <jimmelman@gmail.com>
2018-03-26 20:09:05 +0200

    Add ca-dg to ExportWidget

--- src/app/widget.expand.component.ts
+++ src/app/widget.expand.component.ts
@@ -7,6 +7,8 @@ import { Output }                     from '@angular/core';
 
 // Our models
 import { Datasource }                 from './models';
+import { DatagridInput }              from './models';
+import { DatagridColumn }             from './models';
 
 // Our Services
 import { GlobalFunctionService } 	  from './global-function.service';
@@ -27,9 +29,28 @@ export class WidgetExpandComponent implements OnInit {
     @Output() formWidgetExpandClosed: EventEmitter<string> = new EventEmitter();
 
     currentDatasetLength: number;
-    records: number = 6;
     datasources: Datasource[] = [];
     dataFieldNames: string[] = [];
+    datagriColumns: DatagridColumn[] = [];
+    datagridInput: DatagridInput =
+    {
+        datagriColumns: this.datagriColumns,
+        datagridData: null,
+        datagridPagination: false,
+        datagridPaginationSize: 7,
+        datagridShowHeader: false,
+        datagridShowRowActionMenu: false,
+        datagridShowData: true,
+        datagridShowFooter: true,
+        datagridRowHeight: 12,
+        datagriduserCanChangeProperties: false,
+        datagridShowTotalsRow: false,
+        datagridShowTotalsCol: false,
+        datagridCanEditInCell: false,
+        datagridCanExportData: false,
+        datagridEmptyMessage: 'No records to export'
+    };
+    records: number = 6;
 
     constructor(
         private globalFunctionService: GlobalFunctionService,
@@ -40,26 +61,61 @@ export class WidgetExpandComponent implements OnInit {
         //
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
-        this.globalVariableService.filePath = './assets/data.dataset' +
-            this.selectDatasetID.toString() + '.json';
-        // "../assets/vega-datasets/cars.json";
+        // this.globalVariableService.filePath = './assets/data.dataset' +
+        //     this.selectDatasetID.toString() + '.json';
+        // // "../assets/vega-datasets/cars.json";
 
-        this.globalVariableService.get('').then(i =>
-            {
+        // this.globalVariableService.get('').then(i =>
+        //     {
 
-                // Get fields
-                this.globalVariableService.currentDatasources.forEach(ds => {
-                    if (ds.id = this.selectDatasetID) {
-                        // TODO - remove this, currently datalib reads array as string a,b,c
-                        let x: string = ds.dataFields.toString();
-                        this.dataFieldNames = x.split(',');
-                    }
-                });
-                this.dataset = i;
-                this.currentDatasetLength = i.length;
-            }
-        );
-        console.log('xx23', this.selectWidgetIndex, this.selectDatasetID, this.globalVariableService.filePath)
+        //         // Get fields
+        //         this.globalVariableService.currentDatasources.forEach(ds => {
+        //             if (ds.id == this.selectDatasetID) {
+        //                 // TODO - remove this, currently datalib reads array as string a,b,c
+        //                 let x: string = ds.dataFields.toString();
+        //                 this.dataFieldNames = x.split(',');
+        //             }
+        //         });
+        //         this.dataset = i;
+        //         this.currentDatasetLength = i.length;
+        //     }
+        // );
+        // console.log('xx23', this.selectWidgetIndex, this.selectDatasetID, this.globalVariableService.filePath)
+        this.globalVariableService.getCurrentDataset(
+            this.selectDatasourceID, this.selectDatasetID).then (ca => {
+            this.datagridInput.datagridData = ca.data;
+            if (ca.data.length > 0) {
+                const columns = Object.keys(ca.data[0]);
+                for (var i = 0; i < columns.length; i++) {
+                    this.datagriColumns.push(
+                    {
+                        id: i,
+                        displayName: columns[i],
+                        fieldName: columns[i],
+                        databaseDBTableName: '',
+                        databaseDBFieldName: '',
+                        tooltip: '',
+                        datatype: 'string',
+                        prefix: '',
+                        divideBy: 0,
+                        displayLength: 12,
+                        maxLength: 0,
+                        sortOrder: '',
+                        filter: '',
+                        backgroundColor: '',
+                        color: '',
+                        conditionalFormatColor: '',
+                        nrDataQualityIssues: 0,
+                        maxValue: 0,
+                        minValue: 0,
+                        average: 0,
+                        linkedDashboardID: 0,
+                        linkedDashboardTabID: 0,
+                        isFrozen: false,
+                    });
+                };
+            };
+        });
     }
 
   	clickClose(action: string) {
