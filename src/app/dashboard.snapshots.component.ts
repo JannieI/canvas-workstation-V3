ec6c3d93d729380c21a7de17f5b37388f5ad5906
JannieI <jimmelman@gmail.com>
2018-03-27 13:51:54 +0200

    Refactor Checkpoint form

--- src/app/dashboard.snapshots.component.ts
+++ src/app/dashboard.snapshots.component.ts
@@ -49,7 +49,7 @@ export class DashboardSnapshotsComponent implements OnInit {
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
         this.refreshGrid();
-        
+
     }
 
     clickClose(action: string) {
@@ -63,8 +63,8 @@ export class DashboardSnapshotsComponent implements OnInit {
         // Save the snapshot
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSave', '@Start');
 
-        this.globalVariableService.saveLocal('DashboardSnapshot', { 
-            id: null, 
+        this.globalVariableService.saveLocal('DashboardSnapshot', {
+            id: null,
             dashboardID: this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
             name: snapshotNameInput,
             comment: snapshotCommentInput
@@ -73,25 +73,25 @@ export class DashboardSnapshotsComponent implements OnInit {
         });
 
         this.refreshGrid();
-        
+
     }
 
     refreshGrid() {
         // Refresh the snapshot Grid with the latest info
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSave', '@Start');
-        
+
         this.globalVariableService.getCurrentDashboardSnapshots(
             this.globalVariableService.currentDashboardID).then
               (i => this.currentDashboardSnapshots = i);
 
     }
-    
+
     clickRefreshDashboard(index: number) {
         // Refresh the D to the selected Snapshot, after saving the current D
         this.globalFunctionService.printToConsole(this.constructor.name,'clickRefreshDashboard', '@Start');
 
-        this.globalVariableService.saveLocal('DashboardSnapshot', { 
-            id: null, 
+        this.globalVariableService.saveLocal('DashboardSnapshot', {
+            id: null,
             dashboardID: this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
             name: 'Backup before RollBack',
             comment: 'Saved before rollback was performed'
@@ -101,10 +101,10 @@ export class DashboardSnapshotsComponent implements OnInit {
             this.globalVariableService.refreshCurrentDashboard(
                 'snapshot-clickRefreshDashboard', 1, 2, ''
             );
-            
+
             // Close the form
             this.formDashboardSnapshotsClosed.emit('Rollback');
-        })        
+        })
     }
-        
+
 }
