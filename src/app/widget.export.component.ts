afdc8c2a19caca92f5bfff25d40590af9a82ba7c
JannieI <jimmelman@gmail.com>
2018-03-27 06:55:26 +0200

    Refactor Export Widget

--- src/app/widget.export.component.ts
+++ src/app/widget.export.component.ts
@@ -9,28 +9,19 @@ import { Renderer }                   from '@angular/core';
 import { Router }                     from '@angular/router';
 import { ViewChild }                  from '@angular/core';
 
-// Our models
-import { Datasource }                 from './models';
-
 // Our Services
 import { GlobalFunctionService } 		  from './global-function.service';
 import { GlobalVariableService }      from './global-variable.service';
-
-// Functions
-
-
 
 
 @Component({
     selector: 'widget-export',
     templateUrl: './widget.export.component.html',
     styleUrls: ['./widget.export.component.css']
-  })
-  export class WidgetExportComponent implements OnInit {
+})
+export class WidgetExportComponent implements OnInit {
 
     @Output() formWidgetExportClosed: EventEmitter<string> = new EventEmitter();
-
-    @ViewChild('dragWidget', {read: ElementRef}) dragWidget: ElementRef;  //Vega graph
 
     constructor(
         private globalFunctionService: GlobalFunctionService,
@@ -40,15 +31,24 @@ import { GlobalVariableService }      from './global-variable.service';
     ) {}
 
     ngOnInit() {
-    }
-
-    ngAfterViewInit() {
+        // Initial
+        this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
     }
 
 
   	clickClose(action: string) {
-	  	this.formWidgetExportClosed.emit(action);
-        }
+        // Close the form, no action taken
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickClose', '@Start');
 
-  }
+  	  	this.formWidgetExportClosed.emit(action);
+    }
+
+    clickExport() {
+        // Export the image, and close the file
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickExport', '@Start');
+  
+  	  	this.formWidgetExportClosed.emit('Exported');
+        
+    }
+}
