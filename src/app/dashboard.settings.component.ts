ba7d359ee87563fe5307cb9b1bd24e653a8d34e8
JannieI <jimmelman@gmail.com>
2018-04-17 10:56:49 +0200

    Add .slice() to Settings

--- src/app/dashboard.settings.component.ts
+++ src/app/dashboard.settings.component.ts
@@ -37,7 +37,7 @@ export class DashboardSettingsComponent implements OnInit {
 	) {}
 
     ngOnInit() {
-        this.dashboards = this.globalVariableService.dashboards;
+        this.dashboards = this.globalVariableService.dashboards.slice();
     }
 
     clickClose(action: string) {
