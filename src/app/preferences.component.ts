6f06b71066621454a18380a44dc6030c27800f2f
JannieI <jimmelman@gmail.com>
2018-04-20 07:12:00 +0200

    Update prefs to DB

--- src/app/preferences.component.ts
+++ src/app/preferences.component.ts
@@ -50,18 +50,18 @@ export class PreferencesComponent implements OnInit {
         // Initial
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
-        this.preferenceAutoSync = this.globalVariableService.userPreferences.preferenceAutoSync;
-        this.preferencePaletteHorisontal = this.globalVariableService.userPreferences.preferencePaletteHorisontal;
-        this.preferenceShowOpenStartupMessage = this.globalVariableService.userPreferences.preferenceShowOpenStartupMessage;
-        this.preferenceShowOpenDataCombinationMessage = this.globalVariableService.userPreferences.preferenceShowOpenDataCombinationMessage;
-        this.preferenceShowViewStartupMessage = this.globalVariableService.userPreferences.preferenceShowViewStartupMessage;
-        this.preferenceShowDiscardStartupMessage = this.globalVariableService.userPreferences.preferenceShowDiscardStartupMessage;
-        this.preferenceDefaultTemplate = this.globalVariableService.userPreferences.preferenceDefaultTemplate;
-        this.preferenceDefaultDateformat = this.globalVariableService.userPreferences.preferenceDefaultDateformat;
-        this.preferenceDefaultFolder = this.globalVariableService.userPreferences.preferenceDefaultFolder;
-        this.preferenceDefaultPrinter = this.globalVariableService.userPreferences.preferenceDefaultPrinter;
-        this.preferenceDefaultPageSize = this.globalVariableService.userPreferences.preferenceDefaultPageSize;
-        this.preferenceDefaultPageLayout = this.globalVariableService.userPreferences.preferenceDefaultPageLayout;
+        this.preferenceAutoSync = this.globalVariableService.currentUser.preferenceAutoSync;
+        this.preferencePaletteHorisontal = this.globalVariableService.currentUser.preferencePaletteHorisontal;
+        this.preferenceShowOpenStartupMessage = this.globalVariableService.currentUser.preferenceShowOpenStartupMessage;
+        this.preferenceShowOpenDataCombinationMessage = this.globalVariableService.currentUser.preferenceShowOpenDataCombinationMessage;
+        this.preferenceShowViewStartupMessage = this.globalVariableService.currentUser.preferenceShowViewStartupMessage;
+        this.preferenceShowDiscardStartupMessage = this.globalVariableService.currentUser.preferenceShowDiscardStartupMessage;
+        this.preferenceDefaultTemplate = this.globalVariableService.currentUser.preferenceDefaultTemplate;
+        this.preferenceDefaultDateformat = this.globalVariableService.currentUser.preferenceDefaultDateformat;
+        this.preferenceDefaultFolder = this.globalVariableService.currentUser.preferenceDefaultFolder;
+        this.preferenceDefaultPrinter = this.globalVariableService.currentUser.preferenceDefaultPrinter;
+        this.preferenceDefaultPageSize = this.globalVariableService.currentUser.preferenceDefaultPageSize;
+        this.preferenceDefaultPageLayout = this.globalVariableService.currentUser.preferenceDefaultPageLayout;
     }
 
     clickClose(action: string) {
@@ -83,24 +83,37 @@ export class PreferencesComponent implements OnInit {
         // Save data and Close form
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSave', '@Start');
 
-        let data: UserPreferences = {
-            preferencePaletteHorisontal: this.preferencePaletteHorisontal,
-            preferenceAutoSync: this.preferenceAutoSync,
-            preferenceShowOpenStartupMessage: this.preferenceShowOpenStartupMessage,
-            preferenceShowOpenDataCombinationMessage: this.preferenceShowOpenDataCombinationMessage,
-            preferenceShowViewStartupMessage: this.preferenceShowViewStartupMessage,
-            preferenceShowDiscardStartupMessage: this.preferenceShowDiscardStartupMessage,
-            preferenceDefaultTemplate: this.preferenceDefaultTemplate,
-            preferenceDefaultDateformat: this.preferenceDefaultDateformat,
-            preferenceDefaultFolder: this.preferenceDefaultFolder,
-            preferenceDefaultPrinter: this.preferenceDefaultPrinter,
-            preferenceDefaultPageSize: this.preferenceDefaultPageSize,
-            preferenceDefaultPageLayout: this.preferenceDefaultPageLayout,
-        };
+        // let data: UserPreferences = {
+        //     preferencePaletteHorisontal: this.preferencePaletteHorisontal,
+        //     preferenceAutoSync: this.preferenceAutoSync,
+        //     preferenceShowOpenStartupMessage: this.preferenceShowOpenStartupMessage,
+        //     preferenceShowOpenDataCombinationMessage: this.preferenceShowOpenDataCombinationMessage,
+        //     preferenceShowViewStartupMessage: this.preferenceShowViewStartupMessage,
+        //     preferenceShowDiscardStartupMessage: this.preferenceShowDiscardStartupMessage,
+        //     preferenceDefaultTemplate: this.preferenceDefaultTemplate,
+        //     preferenceDefaultDateformat: this.preferenceDefaultDateformat,
+        //     preferenceDefaultFolder: this.preferenceDefaultFolder,
+        //     preferenceDefaultPrinter: this.preferenceDefaultPrinter,
+        //     preferenceDefaultPageSize: this.preferenceDefaultPageSize,
+        //     preferenceDefaultPageLayout: this.preferenceDefaultPageLayout,
+        // };
+        this.globalVariableService.currentUser.preferencePaletteHorisontal = this.preferencePaletteHorisontal,
+        this.globalVariableService.currentUser.preferenceAutoSync = this.preferenceAutoSync,
+        this.globalVariableService.currentUser.preferenceShowOpenStartupMessage = this.preferenceShowOpenStartupMessage,
+        this.globalVariableService.currentUser.preferenceShowOpenDataCombinationMessage = this.preferenceShowOpenDataCombinationMessage,
+        this.globalVariableService.currentUser.preferenceShowViewStartupMessage = this.preferenceShowViewStartupMessage,
+        this.globalVariableService.currentUser.preferenceShowDiscardStartupMessage = this.preferenceShowDiscardStartupMessage,
+        this.globalVariableService.currentUser.preferenceDefaultTemplate = this.preferenceDefaultTemplate,
+        this.globalVariableService.currentUser.preferenceDefaultDateformat = this.preferenceDefaultDateformat,
+        this.globalVariableService.currentUser.preferenceDefaultFolder = this.preferenceDefaultFolder,
+        this.globalVariableService.currentUser.preferenceDefaultPrinter = this.preferenceDefaultPrinter,
+        this.globalVariableService.currentUser.preferenceDefaultPageSize = this.preferenceDefaultPageSize,
+        this.globalVariableService.currentUser.preferenceDefaultPageLayout = this.preferenceDefaultPageLayout,
 
         // Save globally, and in DB
-        this.globalVariableService.userPreferences = data;
-        this.globalVariableService.saveUserPreferences(data).then(
+        // this.globalVariableService.userPreferences = data;
+        // this.globalVariableService.saveUserPreferences(data).then(
+        this.globalVariableService.saveCanvasUser(this.globalVariableService.currentUser).then(
             res =>
                 {
                     this.globalVariableService.showStatusBarMessage(
