25903315fafa4ee5c870845d72649ed627a0b5b3
JannieI <jimmelman@gmail.com>
2018-05-02 08:36:02 +0200

    Msg drop down menu

--- src/app/collaborate.messagesNew.component.ts
+++ src/app/collaborate.messagesNew.component.ts
@@ -59,7 +59,7 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 dashboardID: 1,
                 dashboardTabID: 1,
                 url: '',
-                iHaveReadThis: false,
+                iHaveReadThis: true,
                 dashboardName: 'Economic Review'
             }
         );
@@ -84,7 +84,7 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 toGroups: [],
                 subject: 'New model',
                 body: 'I have added the transport costs to the new model, but it skews the figures as we now have once-off tonage included for March.  I suggest we leave it out, and just add an annotation.  Let me know by close today so that I can make any adjustmensts prior to our 9 oclock tomorrow...',
-                dashboardID: 1,
+                dashboardID: null,
                 dashboardTabID: 1,
                 url: '',
                 iHaveReadThis: true,
@@ -129,9 +129,9 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 subject: 'Staff turnover looks too high',
                 body: 'Mary, the staff turnover figures for August looks very high, particularly as compared to last year.  At the previous Exco we have discussed the downwards trend.  Can Jerry please review the first graph and let me know.  Oxley',
                 dashboardID: 1,
-                dashboardTabID: 1,
+                dashboardTabID: null,
                 url: '',
-                iHaveReadThis: true,
+                iHaveReadThis: false,
                 dashboardName: 'Economic Review'
             }
         );
@@ -150,10 +150,10 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 toGroups: [],
                 subject: 'Review of upcoming budget',
                 body: 'Hi Johno, I have reviewed the logistics figures are actually included.  It is lower because of the industrial action and holidays.  Also, two b-liners were in for repars.',
-                dashboardID: 1,
+                dashboardID: null,
                 dashboardTabID: 1,
                 url: '',
-                iHaveReadThis: true,
+                iHaveReadThis: false,
                 dashboardName: 'Economic Review'
             }
         );
@@ -172,10 +172,10 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 toGroups: [],
                 subject: 'New model',
                 body: 'Agreed, lets leave it out',
-                dashboardID: 1,
+                dashboardID: null,
                 dashboardTabID: 1,
                 url: '',
-                iHaveReadThis: true,
+                iHaveReadThis: false,
                 dashboardName: 'Economic Review'
             }
         );
@@ -197,7 +197,7 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 dashboardID: 1,
                 dashboardTabID: 1,
                 url: '',
-                iHaveReadThis: true,
+                iHaveReadThis: false,
                 dashboardName: 'Economic Review'
             }
         );
