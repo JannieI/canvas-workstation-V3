a7aa7a8ce4b416cadc19528e877ad26340825177
JannieI <jimmelman@gmail.com>
2018-05-02 07:33:25 +0200

    Msg form, v2

--- src/app/collaborate.messagesNew.component.ts
+++ src/app/collaborate.messagesNew.component.ts
@@ -73,7 +73,29 @@ export class CollaborateMessagesComponentNew implements OnInit {
             {
                 id: 1,
                 threadID: 1,
-                sender: 'JannieI',
+                sender: 'VivK',
+                sentOn: '2017/01/01',
+                recipients: [
+                    {
+                        userID: 'SamH',
+                        readOn: null
+                    }
+                ],
+                toGroups: [],
+                subject: '',
+                body: 'I have added the transport costs to the new model, but it skews the figures as we now have once-off tonage included for March.  I suggest we leave it out, and just add an annotation.  Let me know by close today so that I can make any adjustmensts prior to our 9 oclock tomorrow...',
+                dashboardID: 1,
+                dashboardTabID: 1,
+                url: '',
+                iHaveReadThis: true,
+                dashboardName: 'Economic Review'
+            }
+        );
+        this.canvasMessagesNew.push(
+            {
+                id: 1,
+                threadID: 1,
+                sender: 'JohnatanB',
                 sentOn: '2017/01/01',
                 recipients: [
                     {
@@ -83,7 +105,51 @@ export class CollaborateMessagesComponentNew implements OnInit {
                 ],
                 toGroups: [],
                 subject: 'Review of upcoming budget',
-                body: 'Please review and add missing logistics figures',
+                body: 'Pam, please action',
+                dashboardID: 1,
+                dashboardTabID: 1,
+                url: '',
+                iHaveReadThis: true,
+                dashboardName: 'Economic Review'
+            }
+        );
+        this.canvasMessagesNew.push(
+            {
+                id: 1,
+                threadID: 1,
+                sender: 'OxleyA',
+                sentOn: '2017/01/01',
+                recipients: [
+                    {
+                        userID: 'SamH',
+                        readOn: null
+                    }
+                ],
+                toGroups: [],
+                subject: 'Staff turnover looks too high',
+                body: 'Mary, the staff turnover figures for August looks very high, particularly as compared to last year.  At the previous Exco we have discussed the downwards trend.  Can Jerry please review the first graph and let me know.  Oxley',
+                dashboardID: 1,
+                dashboardTabID: 1,
+                url: '',
+                iHaveReadThis: true,
+                dashboardName: 'Economic Review'
+            }
+        );
+        this.canvasMessagesNew.push(
+            {
+                id: 1,
+                threadID: 1,
+                sender: 'PamH',
+                sentOn: '2017/01/01',
+                recipients: [
+                    {
+                        userID: 'SamH',
+                        readOn: null
+                    }
+                ],
+                toGroups: [],
+                subject: 'Review of upcoming budget',
+                body: 'Hi Johno, I have reviewed the logistics figures are actually included.  It is lower because of the industrial action and holidays.  Also, two b-liners were in for repars.',
                 dashboardID: 1,
                 dashboardTabID: 1,
                 url: '',
