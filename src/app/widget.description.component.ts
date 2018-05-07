5b44b1bf3fe39a5d39368b16501c33af57e2cf88
JannieI <jimmelman@gmail.com>
2018-05-07 15:34:50 +0200

    W Desc form, v1

---
+++ src/app/widget.description.component.ts
@@ -0,0 +1,50 @@
+/*
+ * Visualise page, to view / present Dashboards previously created
+ */
+
+// Angular
+import { Component }                  from '@angular/core';
+import { EventEmitter }               from '@angular/core';
+import { Input }                      from '@angular/core';
+import { OnInit }                     from '@angular/core';
+import { Output }                     from '@angular/core';
+
+// Our Functions
+import { GlobalFunctionService } 	  from './global-function.service';
+
+// Our Services
+import { GlobalVariableService}       from './global-variable.service';
+
+// Models
+import { Dashboard }                  from './models';
+
+@Component({
+    selector: 'widget-description',
+    templateUrl: './widget.description.component.html',
+    styleUrls: ['./widget.description.component.css']
+})
+export class WidgetDescriptionComponent implements OnInit {
+
+    @Input() selectedDashboard: Dashboard;
+    @Output() formDashboardDescriptionClosed: EventEmitter<string> = new EventEmitter();
+
+
+	constructor(
+        private globalFunctionService: GlobalFunctionService,
+        private globalVariableService: GlobalVariableService,
+	) {}
+
+    ngOnInit() {
+        // Initial
+        this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
+
+    }
+
+    clickClose(action: string) {
+        // Close the form, nothing saved
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickClose', '@Start');
+
+		this.formDashboardDescriptionClosed.emit(action);
+    }
+
+}
