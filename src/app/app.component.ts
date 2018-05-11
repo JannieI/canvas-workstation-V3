04158d17a12e34a85c0ed54052aa4afde0b738cd
JannieI <jimmelman@gmail.com>
2018-05-11 10:17:06 +0200

    Amend handleCloseShapeEdit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -892,22 +892,24 @@ export class AppComponent implements OnInit {
         this.globalFunctionService.printToConsole(this.constructor.name,'handleCloseShapeEdit', '@Start');
 
         // Add to Action log
-        this.globalVariableService.actionUpsert(
-            null,
-            this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
-            this.globalVariableService.currentDashboardInfo.value.currentDashboardTabID,
-            changedWidget.id,
-            'Widget',
-            this.newWidget? 'Add' : 'Edit',
-            'App handleCloseShapeEdit',
-            null,
-            null,
-            this.newWidget? null : this.selectedWidget,
-            changedWidget
-        );
+        if (changedWidget != null) {
+            this.globalVariableService.actionUpsert(
+                null,
+                this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
+                this.globalVariableService.currentDashboardInfo.value.currentDashboardTabID,
+                changedWidget.id,
+                'Widget',
+                this.newWidget? 'Add' : 'Edit',
+                'App handleCloseShapeEdit',
+                null,
+                null,
+                this.newWidget? null : this.selectedWidget,
+                changedWidget
+            );
 
-        this.globalVariableService.changedWidget.next(changedWidget);
-
+            this.globalVariableService.changedWidget.next(changedWidget);
+        };
+        
         this.menuOptionClickPostAction();
 
         this.showModalShapeEdit = false;
