87dba6da7002bc5f001eb6b228f6d0a3a933e36f
JannieI <jimmelman@gmail.com>
2018-05-13 19:39:59 +0200

    Add Escape to Add Task

--- src/app/collaborate.taskadd.component.ts
+++ src/app/collaborate.taskadd.component.ts
@@ -2,6 +2,7 @@
 import { Component }                  from '@angular/core';
 import { ElementRef }                 from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -25,8 +26,20 @@ import { GlobalVariableService }      from './global-variable.service';
 export class CollaborateTaskAddComponent implements OnInit {
 
     @Output() formCollaborateTaskAddClosed: EventEmitter<string> = new EventEmitter();
-
     @ViewChild('dragWidget', {read: ElementRef}) dragWidget: ElementRef;  //Vega graph
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     canvasTasks: CanvasTask[] = [];
     dashboardNames: string[] = [];
