d0bacbf167d6abe5da4fdbf2412018accd7e7d1a
JannieI <jimmelman@gmail.com>
2018-05-14 18:35:04 +0200

    Add Escape to Data Summary

--- src/app/data.summary.component.ts
+++ src/app/data.summary.component.ts
@@ -1,9 +1,4 @@
-
-
-
-
-
-            /*
+/*
  * Visualise page, to view / present Dashboards previously created
  */
 
@@ -11,6 +6,7 @@
 import { Component }                  from '@angular/core';
 import { ElementRef }                 from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -34,6 +30,19 @@ export class DataSummaryComponent implements OnInit {
 
     @Output() formDataSummaryClosed: EventEmitter<string> = new EventEmitter();
     @ViewChild('widgetDOM') widgetDOM: ElementRef;
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     finalFields: any = [];
     selectedRow: number = 0;
