7210425650025326088972d6f3875b3af69fb600
JannieI <jimmelman@gmail.com>
2018-05-14 11:40:20 +0200

    Add Escape to D Help

--- src/app/dashboard.help.component.ts
+++ src/app/dashboard.help.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -26,6 +27,19 @@ export class DashboardHelpComponent implements OnInit {
 
     @Output() formDashboardHelpClosed: EventEmitter<string> = new EventEmitter();
 
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
+    
 	constructor(
         private globalFunctionService: GlobalFunctionService,
         private globalVariableService: GlobalVariableService,
