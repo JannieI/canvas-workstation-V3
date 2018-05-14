94ee9ca7e166f1b9226f953619d180a936c82ef2
JannieI <jimmelman@gmail.com>
2018-05-14 18:27:39 +0200

    Add Escape to D Theme

--- src/app/dashboard.theme.component.ts
+++ src/app/dashboard.theme.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -27,6 +28,19 @@ import { DashboardTheme }             from './models';
 export class DashboardThemeComponent implements OnInit {
 
     @Output() formDashboardThemeClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     showTypeDashboard: boolean = false;
     dashboards: Dashboard[];
