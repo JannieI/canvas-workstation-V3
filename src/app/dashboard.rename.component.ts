355166d0209b1a3c0f752dc77874a4d9a80ea897
JannieI <jimmelman@gmail.com>
2018-05-14 11:48:00 +0200

    Add Escape to D Rename

--- src/app/dashboard.rename.component.ts
+++ src/app/dashboard.rename.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -25,6 +26,19 @@ export class DashboardRenameComponent implements OnInit {
 
     @Output() formDashboardRenameClosed: EventEmitter<string> = new EventEmitter();
 
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
+    
     errorMessage: string = '';
     filterCreatedBy: string;
     filterDatasource: string;
