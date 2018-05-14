cf09c32efd328d569ab0a441462dc87556e15af8
JannieI <jimmelman@gmail.com>
2018-05-14 18:33:25 +0200

    Add Escape to Data popup

--- src/app/data.popup.component.ts
+++ src/app/data.popup.component.ts
@@ -8,6 +8,7 @@
 import { Component }                  from '@angular/core';
 import { ElementRef }                 from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                     from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -44,6 +45,19 @@ export class DataPopupComponent implements OnInit {
     @ViewChild('typeDropdown') typeDropdown: ElementRef;
     @ViewChild('typeTransformationDropdown') typeTransformationDropdown: ElementRef;
     @ViewChild('transformations') transformations: ElementRef;
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     // datasources: Datasource[];
     addNewTransformation: boolean = false;
