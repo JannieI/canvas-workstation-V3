868dcfd3b07e4d24a4cc81dc27590506c74ea32a
JannieI <jimmelman@gmail.com>
2018-05-14 11:13:30 +0200

    Add Escape to D Comments

--- src/app/dashboard.comments.component.ts
+++ src/app/dashboard.comments.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -31,6 +32,19 @@ export class DashboardCommentsComponent implements OnInit {
     @Output() formDashboardCommentsClosed: EventEmitter<string> = new EventEmitter();
     @Input() selectedWidgetID: number;
 
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
+    
     canvasComments: CanvasComment[] = [];
     datagridColumns: string[] =["id"]
     commentText: string;
