291ee49a05066cbb10abdc998a0a84d55b15fa5c
JannieI <jimmelman@gmail.com>
2018-05-15 10:07:08 +0200

    Add Escape to W Expand

--- src/app/widget.expand.component.ts
+++ src/app/widget.expand.component.ts
@@ -1,6 +1,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -26,6 +27,19 @@ export class WidgetExpandComponent implements OnInit {
     @Input() selectDatasetID: number;
     @Input() selectDatasourceID: number;
     @Output() formWidgetExpandClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     currentDatasetLength: number;
     datagridColumns: string[];
