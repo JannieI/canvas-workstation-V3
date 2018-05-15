6c29467fa018fce2ea8dcade62eb76e9110bb759
JannieI <jimmelman@gmail.com>
2018-05-15 12:28:27 +0200

    Add sort to Open Dashboards

--- src/app/dashboard.open.component.ts
+++ src/app/dashboard.open.component.ts
@@ -58,7 +58,17 @@ export class DashboardOpenComponent implements OnInit {
         //
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
-        this.dashboards = this.globalVariableService.dashboards.slice();
+        this.dashboards = this.globalVariableService.dashboards.slice().sort((n1,n2) => {
+            if (n1.name > n2.name) {
+                return 1;
+            };
+        
+            if (n1.name < n2.name) {
+                return -1;
+            };
+        
+            return 0;
+        });
         this.isFirstTimeDashboardOpen = this.globalVariableService.currentUser.preferenceShowOpenStartupMessage;
     }
 
