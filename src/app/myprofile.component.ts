f809b44927dae19757aafedde6ff9bf490884018
JannieI <jimmelman@gmail.com>
2018-05-15 09:50:26 +0200

    Add Escape to My Profile

--- src/app/myprofile.component.ts
+++ src/app/myprofile.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -26,6 +27,19 @@ import { Dashboard }                  from './models';
 export class MyProfileComponent implements OnInit {
 
     @Output() formDashboardMyProfileClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     currentData: any[] = [];
     dataFieldNames: number[] = [];
