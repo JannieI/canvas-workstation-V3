460e9f21ec9456915813fe8b6cbfa6f3ab1895aa
JannieI <jimmelman@gmail.com>
2018-05-15 09:51:37 +0200

    Add Escape to Shape Delete

--- src/app/shape.delete.component.ts
+++ src/app/shape.delete.component.ts
@@ -2,6 +2,7 @@
 import { Component }                  from '@angular/core';
 import { ElementRef }                 from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -27,6 +28,19 @@ export class ShapeDeleteComponent implements OnInit {
 
     @Input() currentShapeSpec: any;
     @Output() formShapeDeleteClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose();
+            return;
+        };
+
+    }
 
     @ViewChild('dragWidget', {read: ElementRef}) dragWidget: ElementRef;  //Vega graph
 
