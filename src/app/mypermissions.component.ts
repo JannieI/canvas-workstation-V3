c0ff0efb7a0edf718665a905d9053ccbf1f223b8
JannieI <jimmelman@gmail.com>
2018-05-15 09:49:57 +0200

    Add Escape to My Permissions

--- src/app/mypermissions.component.ts
+++ src/app/mypermissions.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -33,6 +34,19 @@ export class MyPermissionsComponent implements OnInit {
 
     @Input() selectedDashboard: Dashboard;
     @Output() formDashboardMyPermissionsClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     dashboardPermissions: localDashboardPermission[];
     groupID: number;
