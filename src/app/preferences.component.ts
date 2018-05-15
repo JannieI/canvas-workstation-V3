001235306156daca9baacbc420b862c953b1b00c
JannieI <jimmelman@gmail.com>
2018-05-15 09:50:52 +0200

    Add Escape to Preferences

--- src/app/preferences.component.ts
+++ src/app/preferences.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -26,6 +27,19 @@ import { Dashboard }                  from './models';
 export class PreferencesComponent implements OnInit {
 
     @Output() formDashboardPreferencesClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
 
     preferenceAutoSync: boolean;
     preferencePaletteHorisontal: boolean;
