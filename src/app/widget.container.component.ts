3c4644623334160070e6465dcd6bbf03a1122fcd
JannieI <jimmelman@gmail.com>
2018-05-15 10:32:51 +0200

    Add Escape to Widget Container

--- src/app/widget.container.component.ts
+++ src/app/widget.container.component.ts
@@ -5,6 +5,7 @@
 // Angular
 import { Component }                  from '@angular/core';
 import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
 import { Input }                      from '@angular/core';
 import { OnInit }                     from '@angular/core';
 import { Output }                     from '@angular/core';
@@ -25,6 +26,19 @@ export class WidgetContainerComponent implements OnInit {
 
     @Output() formWidgetContainerClosed: EventEmitter<Widget> = new EventEmitter();
     @Input() selectedWidget: Widget;
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose();
+            return;
+        };
+
+    }
 
     localWidget: Widget;                            // W to modify, copied from selected
 
