f319105b913cd383c965df50d4ef4f9dd158b619
JannieI <jimmelman@gmail.com>
2018-05-16 11:47:33 +0200

    Add Edit Role permission check to clickMenuTableEdit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3229,6 +3229,20 @@ export class AppComponent implements OnInit {
         // Edits the selected Table
         this.globalFunctionService.printToConsole(this.constructor.name,'clickMenuTableEdit', '@Start');
 
+        // Permissions
+        if (!this.globalVariableService.currentUser.isDashboardEditor
+            &&
+            !this.globalVariableService.currentUser.isAdministrator) {
+            this.showMessage(
+                'You do not have Edit Permissions (role must be added)',
+                'StatusBar',
+                'Warning',
+                3000,
+                ''
+            );
+            return;
+        };
+
         // Has to be in editMode
         if (!this.editMode) {
             this.showMessage(
