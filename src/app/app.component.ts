e212f0d4747b30875849e3d0a2c7cf4bca44f04e
JannieI <jimmelman@gmail.com>
2018-05-16 14:11:39 +0200

    Add Editor role to Sh Del

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3955,6 +3955,20 @@ export class AppComponent implements OnInit {
         // Delete a Shape
         this.globalFunctionService.printToConsole(this.constructor.name,'clickMenuShapeDelete', '@Start');
 
+        // Permissions
+        if (!this.globalVariableService.currentUser.isDashboardEditor
+            &&
+            !this.globalVariableService.currentUser.isAdministrator) {
+            this.showMessage(
+                'You do not have Edit Permissions (role must be added)',
+                'StatusBar',
+                'Warning',
+                3000,
+                ''
+            );
+            return;
+        };
+
         // Has to be in editMode
         if (!this.editMode) {
             this.showMessage(
