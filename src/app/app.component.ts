ca6110087c763c99b14df55dfa2f674214ccc63d
JannieI <jimmelman@gmail.com>
2018-05-16 14:10:29 +0200

    Add Editor role to W Delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3221,6 +3221,20 @@ export class AppComponent implements OnInit {
         // Delete the selected W
         this.globalFunctionService.printToConsole(this.constructor.name,'clickMenuWidgetDelete', '@Start');
 
+        // Permissions
+        if (!this.globalVariableService.currentUser.isDashboardEditor
+            &&
+            !this.globalVariableService.currentUser.isAdministrator) {
+            this.showMessage(
+                'You do not have Edit Permissions (role must be added)',
+                'StatusBar',
+                'Warning',
+                3000,
+                ''
+            );
+            return;
+        };
+
         // Has to be in editMode
         if (!this.editMode) {
             this.showMessage(
