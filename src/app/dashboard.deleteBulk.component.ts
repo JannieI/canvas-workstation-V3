1b09ad8e3ae70bfbdc2df1792b235ce3b6121c17
JannieI <jimmelman@gmail.com>
2018-05-26 10:55:39 +0200

    Amend clickDelete

--- src/app/dashboard.deleteBulk.component.ts
+++ src/app/dashboard.deleteBulk.component.ts
@@ -85,8 +85,12 @@ export class DashboardDeleteBulkComponent implements OnInit {
         };
 
         // TODO - remove later on!!
-        if (id < 9  || id == 40  ||  id == 41) {
-            alert('Dont delete 1-8, 40, 41 while testing !')
+        if (
+            (id < 9  || id == 40  ||  id == 41)  
+            &&  
+            this.globalVariableService.currentDashboardInfo.value.currentDashboardState == 'Complete'
+            ) {
+            alert('Dont delete Complete version of ids 1-8, 40, 41 while testing !')
             return;
         };
         
