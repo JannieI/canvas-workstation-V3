8ffd7cf74166509ec124024ebb385072f432fd27
JannieI <jimmelman@gmail.com>
2018-06-15 19:07:06 +0200

    Amend clickDSPreview

--- src/app/data.direct.import.component.ts
+++ src/app/data.direct.import.component.ts
@@ -52,8 +52,8 @@ export class DataDirectImportComponent implements OnInit {
     dataFieldNames: string[];
     dataFieldTypes: string[] = [];
     errorMessage: string = "";
-    fileName: string = '';
-    folderName: string = '';
+    fileName: string = 'datasource.sharePrices.json';
+    folderName: string = './assets/';
     finalFields: any = [];
     selectedFile: boolean = true;
     currentDatasetName: string;
@@ -73,13 +73,6 @@ export class DataDirectImportComponent implements OnInit {
         this.datasources = this.globalVariableService.datasources.slice();
     }
 
-    clickFileBrowse() {
-        //
-        this.globalFunctionService.printToConsole(this.constructor.name,'clickFileBrowse', '@Start');
-
-        // TODO alert('Later: File component to browse ...')
-    }
-
     clickDSPreview() {
         // Load the new DS in the ID section, and show in Preview area
         this.globalFunctionService.printToConsole(this.constructor.name,'clickDSPreview',           '@Start');
@@ -87,20 +80,7 @@ export class DataDirectImportComponent implements OnInit {
         // Reset
         this.errorMessage = '';
 
-        // // Get the folder and file, setting some defaults
-        // if (this.folderName == ''  ||  this.folderName == undefined) {
-        //     this.folderName = './assets/vega-datasets/';
-        // }
-        // if (this.fileName ==''  ||  this.fileName == undefined) {
-        //     this.fileName = 'stocks.csv';
-        // };
-
-        // Load synchronously
-        var csv_data = dl.load({url: this.folderName + this.fileName});
-        console.log('DataPopup clickDSPreview LOAD data start:', this.folderName, this.fileName)
-        // let fileFolder: string = './assets/vega-datasets/';
         let filePath: string = this.folderName + this.fileName;
-
         let fileSuffix = this.fileName.substr(this.fileName.lastIndexOf('.')+1,this.fileName.length-this.fileName.indexOf('.'));
 
         if (fileSuffix == 'json') {
@@ -115,21 +95,10 @@ export class DataDirectImportComponent implements OnInit {
                 }
             });
         };
-        if (fileSuffix == 'csv') {
-            dl.csv({url: filePath}, {}, (err, currentData) => {
-                if (err) {
-                    this.errorMessage = err.status + ':' + err.statusText;
-                    console.log('DataPopup clickDSPreview error on load', err)
-                } else {
-                    // Callback
-                    this.fileLoadedCallback(fileSuffix, currentData);
-                }
-            });
-        };
 
         // Message when file type unknown
-        if (fileSuffix != 'json'  &&  fileSuffix != 'csv') {
-            this.errorMessage = 'Unknown file type';
+        if (fileSuffix != 'json') {
+            this.errorMessage = 'File type must be .json';
         };
     }
 
@@ -142,116 +111,11 @@ export class DataDirectImportComponent implements OnInit {
 
         // Load
         console.log('')
-        console.log('DataPopup clickDSPreview LOAD start:')
+        console.log('DataPopup clickDSPreview LOAD start:', currentData)
         this.currentData = currentData;
-        // this.globalVariableService.datasets.push(
-        //     {
-        //         datasourceID : 3,
-        //         data: currentData
-        //     }
-        // );
-        currentData = [];
-        console.log('DataPopup clickDSPreview      data rows', this.currentData.length)
-        console.log('DataPopup clickDSPreview      END load: ', (Date.now() - startNow) / 1000)
-
-        // Fields
-        console.log('')
-        console.log('DataPopup clickDSPreview FIELDS start:')
-        startNow = Date.now()
-        var dataTypes = dl.type.all(this.currentData)
-        this.dataFieldNames = Object.keys(dataTypes);
-        console.log('DataPopup clickDSPreview      fields', this.dataFieldNames)
-        for (var i = 0; i < this.dataFieldNames.length; i++) {
-            console.log('     ', i, this.dataFieldNames[i])
-        }
-        console.log('DataPopup clickDSPreview      END fields: ', (Date.now() - startNow) / 1000)
-
-        // Types
-        console.log('')
-        console.log('DataPopup clickDSPreview TYPES start:')
-        startNow = Date.now()
-        this.dataFieldTypes = [];
-        console.log('DataPopup clickDSPreview      types');
-        for (var i = 0; i < this.dataFieldNames.length; i++) {
-            this.dataFieldTypes.push(dataTypes[ this.dataFieldNames[i] ] );
-            console.log('DataPopup clickDSPreview      ', i, this.dataFieldTypes[i])
-        }
-        console.log('DataPopup clickDSPreview      END types: ', (Date.now() - startNow) / 1000)
-
-        // Lengths
-        console.log('')
-        console.log('DataPopup clickDSPreview LENGTHS start:')
-        startNow = Date.now()
-        this.dataFieldLengths = [];
-        console.log('DataPopup clickDSPreview      lengths');
-        for (var i = 0; i < this.dataFieldTypes.length; i++) {
-            if (this.dataFieldTypes[i] == 'string'  ||  this.dataFieldTypes[i] == 'date') {
-                this.dataFieldLengths.push(25);
-            } else {
-                this.dataFieldLengths.push(12);
-            }
-            console.log('DataPopup clickDSPreview      ', i, this.dataFieldLengths[i])
-        }
-        console.log('DataPopup clickDSPreview      END lengths: ', (Date.now() - startNow) / 1000)
-
-        // Sort
-        console.log('')
-        console.log('DataPopup clickDSPreview SORT start:')
-        startNow = Date.now()
-        this.currentData.sort(dl.comparator(['+symbol', '-price']));
-        console.log('DataPopup clickDSPreview      END sort: ', (Date.now() - startNow) / 1000)
-
-        // Group By
-        console.log('')
-        console.log('DataPopup clickDSPreview GROUPBY start:')
-        startNow = Date.now()
-        this.dataArray = dl.groupby('symbol')
-            .summarize( [
-                {name: 'symbol', ops: ['valid']},
-                {name: 'price',  ops: ['sum', 'median'], as: ['s', 'm']}
-                ] )
-            .execute(this.currentData);
-        console.log('DataPopup clickDSPreview      groupby', this.dataArray)
-        console.log('DataPopup clickDSPreview      END groupby: ', (Date.now() - startNow) / 1000)
-
-        // Get Unique Symbols
-        console.log('')
-        console.log('DataPopup clickDSPreview UNIQUE start:')
-        startNow = Date.now()
-        var dataUniqueInColumn = dl.unique(this.currentData);
-        console.log('DataPopup clickDSPreview      unique', dataUniqueInColumn)
-        console.log('DataPopup clickDSPreview      END unique: ', (Date.now() - startNow) / 1000)
-
-        // Get Unique Symbols 2
-        console.log('')
-        console.log('DataPopup clickDSPreview UNIQUE 2 start:')
-        startNow = Date.now()
-        dataUniqueInColumn = dl.groupby('symbol')
-            .summarize( [
-                {name: 'symbol', ops: ['values']}
-                ] )
-            .execute(this.currentData);
-        console.log('DataPopup clickDSPreview      unique', dataUniqueInColumn)
-        console.log('DataPopup clickDSPreview      END unique: ', (Date.now() - startNow) / 1000)
-
-        // Preview
-        console.log('')
-        console.log('DataPopup clickDSPreview PREVIEW start:')
-        startNow = Date.now()
-        console.log('DataPopup clickDSPreview         END preview: ', (Date.now() - startNow) / 1000)
-
-        // No DS currently selected
-        this.currentDatasetName = '';
 
     }
  
-    clickFileSaveTransformation() {
-        //
-        this.globalFunctionService.printToConsole(this.constructor.name,'clickFileSaveTransformation', '@Start');
-
-        // TODO - add code to Save the name for next time ...
-    }
-
     clickClose(action: string) {
         //
         this.globalFunctionService.printToConsole(this.constructor.name,'clickClose', '@Start');
