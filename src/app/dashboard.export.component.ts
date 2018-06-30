658b6f35b90c6e3409f1d6a35c50e805611dd4ab
JannieI <jimmelman@gmail.com>
2018-06-30 20:53:49 +0200

    Create D Export, v1

---
+++ src/app/dashboard.export.component.ts
@@ -0,0 +1,61 @@
+/*
+ * Visualise page, to view / present Dashboards previously created
+ */
+
+// Angular
+import { Component }                  from '@angular/core';
+import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
+import { Input }                      from '@angular/core';
+import { OnInit }                     from '@angular/core';
+import { Output }                     from '@angular/core';
+
+// Our Functions
+import { GlobalFunctionService } 	  from './global-function.service';
+
+// Our Services
+import { GlobalVariableService}       from './global-variable.service';
+
+// Models
+import { Dashboard }                  from './models';
+
+@Component({
+    selector: 'dashboard-export',
+    templateUrl: './dashboard.export.component.html',
+    styleUrls: ['./dashboard.export.component.css']
+})
+export class DashboardexportComponent implements OnInit {
+
+    @Output() formDashboardExportClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
+
+    showTypeDashboard: boolean = false;
+    dashboards: Dashboard[];
+
+	constructor(
+        private globalFunctionService: GlobalFunctionService,
+        private globalVariableService: GlobalVariableService,
+	) {}
+
+    ngOnInit() {
+        this.dashboards = this.globalVariableService.dashboards.slice();
+    }
+
+    clickClose(action: string) {
+        console.log('clickClose')
+
+		this.formDashboardExportClosed.emit(action);
+    }
+}
