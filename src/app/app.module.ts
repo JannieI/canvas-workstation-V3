c6ee51aea385a9b57441352ff6b8e23ed94aa7bb
JannieI <jimmelman@gmail.com>
2018-07-09 14:49:57 +0200

    Depricate D import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,22 +28,21 @@ import { DashboardSaveComponent }     from './dashboard.save.component';
 import { DashboardSaveAsComponent }   from './dashboard.saveas.component';
 import { DashboardDiscardComponent }  from './dashboard.discard.component';
 import { DashboardRenameComponent }   from './dashboard.rename.component';
-import { DashboardSnapshotsComponent }      from './dashboard.snapshots.component';
-import { DashboardDescriptionComponent }    from './dashboard.description.component';
+import { DashboardSnapshotsComponent }          from './dashboard.snapshots.component';
+import { DashboardDescriptionComponent }        from './dashboard.description.component';
 import { DashboardShareComponent }    from './dashboard.share.component';
 import { DashboardTagsComponent }     from './dashboard.tags.component';
 import { DashboardThemeComponent }    from './dashboard.theme.component';
 import { DashboardTemplateComponent } from './dashboard.template.component';
 import { DashboardScheduleComponent } from './dashboard.schedule.component';
-import { DashboardScheduleEditComponent }   from './dashboard.scheduleEdit.component';
+import { DashboardScheduleEditComponent }       from './dashboard.scheduleEdit.component';
 import { DashboardDeleteComponent }   from './dashboard.delete.component';
-import { DashboardDeleteBulkComponent }     from './dashboard.deleteBulk.component';
+import { DashboardDeleteBulkComponent }         from './dashboard.deleteBulk.component';
 import { DashboardCommentsComponent } from './dashboard.comments.component';
 import { DashboardPrintComponent }    from './dashboard.print.component';
 import { DashboardTabComponent }      from './dashboard.tab.component';
 import { DashboardHelpComponent }     from './dashboard.help.component';
 import { DashboardTreeviewComponent } from './dashboard.treeview.component';
-import { DashboardImportComponent }   from './dashboard.import.component';
 import { DashboardExportComponent }   from './dashboard.export.component';
 import { DashboardSubscribeComponent }         from './dashboard.subscribe.component';
 import { DashboardDataQualityComponent }       from './dashboard.dataquality.component';
@@ -74,47 +73,47 @@ import { ShapeEditComponent }         from './shape.editor.component';
 import { ShapeDeleteComponent }       from './shape.delete.component';
 
 import { DataDirectFileComponent }    from './data.direct.file.component';
-import { DataDirectQueryBuilderComponent }  from './data.direct.queryBuilder.component';
-import { DataDirectSQLEditorComponent }      from './data.direct.sqlEditor.component';
+import { DataDirectQueryBuilderComponent }      from './data.direct.queryBuilder.component';
+import { DataDirectSQLEditorComponent }         from './data.direct.sqlEditor.component';
 import { DataDirectNoSQLComponent }   from './data.direct.Nosql.component';
 import { DataDirectServiceComponent } from './data.direct.service.component';
 import { DataDirectWebComponent }     from './data.direct.web.component';
 import { DataDirectImportComponent }  from './data.direct.import.component';
 import { DataDirectExportComponent }  from './data.direct.export.component';
 
-import { DataManagedConnectionComponent }    from './data.managed.connection.component';
-import { DataManagedQueryBuilderComponent }  from './data.managed.queryBuilder.component';
-import { DataManagedSQLEditorComponent }     from './data.managed.sqlEditor.component';
-import { DataManagedGraphQLEditorComponent } from './data.managed.graphqlEditor.component';
-import { DataManagedOverlayEditorComponent } from './data.managed.overlayEditor.component';
-import { DataManagedNoSQLEditorComponent }   from './data.managed.noSqlEditor.component';
-import { DataManageDataQualityComponent }    from './data.managed.dataQuality.component';
-import { DataManageDataOwnershipComponent }  from './data.managed.dataOwnership.component';
-import { DataManagedDataDictionaryComponent } from './data.managed.dataDictionary.component';
-import { DataManagedBusGlossaryComponent }    from './data.managed.busGlossary.component';
+import { DataManagedConnectionComponent }       from './data.managed.connection.component';
+import { DataManagedQueryBuilderComponent }     from './data.managed.queryBuilder.component';
+import { DataManagedSQLEditorComponent }        from './data.managed.sqlEditor.component';
+import { DataManagedGraphQLEditorComponent }    from './data.managed.graphqlEditor.component';
+import { DataManagedOverlayEditorComponent }    from './data.managed.overlayEditor.component';
+import { DataManagedNoSQLEditorComponent }      from './data.managed.noSqlEditor.component';
+import { DataManageDataQualityComponent }       from './data.managed.dataQuality.component';
+import { DataManageDataOwnershipComponent }     from './data.managed.dataOwnership.component';
+import { DataManagedDataDictionaryComponent }   from './data.managed.dataDictionary.component';
+import { DataManagedBusGlossaryComponent }      from './data.managed.busGlossary.component';
 
-import { DataTransformationComponent }       from './data.transformation.component';
-import { DataEditDatasourceComponent }       from './data.editDatasource.component';
-import { DataDatasourceOverviewComponent }   from './data.datasourceOverview.component';
+import { DataTransformationComponent }          from './data.transformation.component';
+import { DataEditDatasourceComponent }          from './data.editDatasource.component';
+import { DataDatasourceOverviewComponent }      from './data.datasourceOverview.component';
 import { DataCombinationComponent }   from './data.combination.component';
-import { DataCombinationAppendComponent }    from './data.combination.append.component';
+import { DataCombinationAppendComponent }       from './data.combination.append.component';
 import { DatasourceShareComponent }   from './data.share.component';
 import { DataDictionaryComponent }    from './data.dictionary.component';
-import { DataBusinessGlossaryComponent }     from './data.businessGlossary.component';
+import { DataBusinessGlossaryComponent }        from './data.businessGlossary.component';
 import { DataSummaryComponent }       from './data.summary.component';
-import { DataDatasourceUsageComponent }      from './data.datasourceUsage.component';
+import { DataDatasourceUsageComponent }         from './data.datasourceUsage.component';
 import { DataRefreshOnceComponent }   from './data.refresh.once.component';
 import { DataRefreshRepeatComponent }   from './data.refresh.repeat.component';
-import { DataDeleteDatasourceComponent }     from './data.deleteDatasource.component';
+import { DataDeleteDatasourceComponent }        from './data.deleteDatasource.component';
 
 import { HelpComponent }              from './help.component';
 
-import { CollaborateMessagesComponent }     from './collaborate.messages.component';
-import { CollaborateTaskAddComponent }      from './collaborate.taskadd.component';
-import { CollaborateTasksComponent }        from './collaborate.tasks.component';
-import { CollaborateAuditTrailComponent }   from './collaborate.auditTrail.component';
-import { CollaborateSendMessageComponent }  from './collaborate.sendmessage.component';
-import { CollaborateSendEmailComponent }    from './collaborate.sendemail.component';
+import { CollaborateMessagesComponent }         from './collaborate.messages.component';
+import { CollaborateTaskAddComponent }          from './collaborate.taskadd.component';
+import { CollaborateTasksComponent }            from './collaborate.tasks.component';
+import { CollaborateAuditTrailComponent }       from './collaborate.auditTrail.component';
+import { CollaborateSendMessageComponent }      from './collaborate.sendmessage.component';
+import { CollaborateSendEmailComponent }        from './collaborate.sendemail.component';
 import { CollaborateMessageContentComponent }   from './collaborate.messageContent.component';
 
 import { UserPaletteButtonBarComponent}         from './user.palette.buttonbar.component';
@@ -174,7 +173,6 @@ import { StatusbarComponent }         from './statusbar.component';
         DashboardHelpComponent,
         DashboardTreeviewComponent,
         DashboardSubscribeComponent,
-        DashboardImportComponent,
         DashboardExportComponent,
         DashboardDataQualityComponent,
         DashboardUsagestatsComponent,
