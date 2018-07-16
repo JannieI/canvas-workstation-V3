abef89110b407e277f60433fae35db300401ed1f
JannieI <jimmelman@gmail.com>
2018-07-16 19:01:33 +0200

    Cleanup

--- src/app/data.direct.fileSpreadsheet.component.ts
+++ src/app/data.direct.fileSpreadsheet.component.ts
@@ -1,5 +1,5 @@
 /*
- * Create a new datasources from a File.
+ * Create a new datasources from a Spreadsheet.
  */
 
 // Angular
@@ -38,39 +38,28 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
 
         // Known ones
         if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
-            this.clickClose('Close');
+            this.clickClose();
             return;
         };
 
     }
 
+    canSave: boolean = false;
+    errorMessage: string = "";
+    fields: string[] = [];
+    fileName: string = '';
+    headerRow: string = '0';
+    loadedFile: any;
+    newDescription: string = '';
+    newName: string = '';
+    reader = new FileReader();
+    savedMessage: string = '';
+    theFile: any;
+    selectedWorksheetRowIndex: number;
+    worksheetColumns: any[] = [];
     worksheetData: any = [];
     worksheetDataFull: any = [];
-    currentDatasetName: string;
-    currentDatasources: Datasource[] = [];
-    dataArray: any;
-    dataFieldLengths: number[] = [];
-    dataFieldNames: string[] = ['ColA','ColB'];
-    dataFieldTypes: string[] = [];
-    datasources: Datasource[];
-    newName: string = '';
-    newDescription: string = '';
-    errorMessage: string = "";
-    existingDSName: string = '';
-    fileName: string = '';
-    finalFields: any = [];
-    folderName: string = '';
-    headerRow: string = '0';
-    reader = new FileReader();
-    selectedFile: boolean = true;
-    theFile: any;
     worksheets: string[] = [];
-    worksheetColumns: any[] = [];
-    fields: string[] = [];
-    loadedFile: any;
-    canSave: boolean = false;
-    selectedWorksheetRowIndex: number;
-    savedMessage: string = '';
 
 	constructor(
         private globalFunctionService: GlobalFunctionService,
@@ -85,9 +74,6 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
             this.fileName = this.selectedDatasource.fileName;
         };
 
-        // Load from global variables
-        this.currentDatasources = this.globalVariableService.currentDatasources.slice();
-        this.datasources = this.globalVariableService.datasources.slice();
     }
 
     clickFileBrowse() {
@@ -117,7 +103,6 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
         // Access and handle the files
         this.theFile = inp.files[0];
         this.fileName = this.theFile.name
-        console.warn('xx pre readAsBinaryString', this.theFile, this.theFile.name, this.theFile.type, this.theFile.size, this.theFile.lastModifiedDate, this.theFile.lastModifiedDate.toLocaleDateString())
 
         // Read file as Binary
         this.reader.onerror = this.errorHandler;
@@ -126,7 +111,6 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
 
         // Read in the image file as a data URL.
         this.reader.readAsBinaryString(this.theFile);
-        console.warn('xx Post readAsBinaryString')
     }
 
     inspectFile(loadedFile) {
@@ -142,7 +126,6 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
             }
         };
         this.globalVariableService.getTributaryInspect(specification).then(res => {
-            console.warn('xx inspect from Trib', res) 
             this.worksheets = [];
             res.forEach(row => {
                 this.worksheets.push(row.name);
@@ -151,8 +134,7 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
 
             if (this.worksheets.length > 0) {
                 this.clickWorksheet(0);
-            }
-            console.warn('xx fields', this.worksheetColumns)
+            };
         });
 
 
@@ -204,14 +186,13 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
 
         // Can Add now
         this.canSave = true;
-        console.warn('xx fields ', this.fields, index)
 
         // Reset
         this.errorMessage = '';
 
         // Validation
         if (this.fileName == ''  ||  this.fileName == null) {
-            this.errorMessage = 'Please enter a file name  OR  select one using the Browse button';
+            this.errorMessage = 'Please select a file using the Browse button';
             return;
         };
         // skip_rows = [number = rows to skip, string = ignore rows that starts with this]
@@ -225,6 +206,7 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
         let specification: any;
         let lastFive: string = this.fileName.slice(-5);
         if (lastFive.toLowerCase() == '.xlsx') {
+            console.warn('xx xlsx')
             specification = {
                 "source": {
                     "connector": "tributary.connectors.spreadsheet:XlsxConnector",
@@ -240,6 +222,7 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
             let lastFour: string = this.fileName.slice(-4);
         
             if (lastFour.toLowerCase() == '.xls') {
+                console.warn('xx xls')
 
                 specification = {
                     "source": {
@@ -253,6 +236,7 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
                     }
                 };
             } else if (lastFour.toLowerCase() == '.ods') {
+                console.warn('xx ods')
 
                 specification = {
                     "source": {
@@ -271,151 +255,28 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
             };
         };
         this.globalVariableService.getTributaryData(specification).then(res => {
-            console.warn('xx data from Trib', res) 
+
+            // Fill the data
             this.worksheetData = res.slice(0,10);
             this.worksheetDataFull = res;
         });
 
     }
 
-    clickClose(action: string) {
-        //
+    clickClose() {
+        // Close the form, nothing saved
         this.globalFunctionService.printToConsole(this.constructor.name,'clickClose', '@Start');
 
         this.formDataDirectFileSpreadsheetClosed.emit(null);
 
     }
 
-    clickAdd(action: string) {
+    clickAdd() {
         // Add the DS, with data, to the DB
         this.globalFunctionService.printToConsole(this.constructor.name,'clickAdd', '@Start');
 
         // Reset
         this.errorMessage = '';
-
-        // let today = new Date();
-
-        // // New Datasource
-        // let newData: Datasource =  {
-        //     id: null,
-        //     name: this.fileName,
-        //     username: '',
-        //     password: '',
-        //     type: 'File',
-        //     subType: 'CSV',
-        //     typeVersion: 'Comma-Separated',
-        //     description: 'Hard coded name',
-        //     createdBy: this.globalVariableService.currentUser.userID,
-        //     createdOn: today,
-        //     createMethod: 'directFile',
-        //     editor: '',
-        //     dateEdited: null,
-        //     refreshedBy: this.globalVariableService.currentUser.userID,
-        //     refreshedOn: today,
-        //     dataFieldIDs: [0],
-        //     dataFields: this.dataFieldNames,
-        //     dataFieldTypes: this.dataFieldTypes,
-        //     dataFieldLengths: this.dataFieldLengths,
-        //     parameters: 'None',
-        //     folder: '',
-        //     fileName: this.fileName,
-        //     excelWorksheet: '',
-        //     transposeOnLoad: false,
-        //     startLineNr: 1,
-        //     csvSeparationCharacter: '',
-        //     csvQuotCharacter: '',
-        //     webUrl: '',
-        //     webTableIndex: '',
-        //     connectionID: 0,
-        //     dataTableID: 0,
-        //     nrWidgets: 0,
-        //     databaseName: '',
-        //     port: '',
-        //     serverType: '',
-        //     serverName: '',
-        //     dataTableName: '',
-        //     dataSQLStatement: '',
-        //     dataNoSQLStatement: '',
-        //     dataNeo4jStatement: '',
-        //     dataGraphQLStatement: '',
-        //     businessGlossary: '',
-        //     dataDictionary: ''
-
-        // };
-
-        // // General var with name - used in *ngIF, etc
-        // if (this.existingDSName == '') {
-        //     this.currentDatasetName = this.fileName;
-        // } else {
-        //     this.currentDatasetName = this.existingDSName;
-        // };
-        // this.existingDSName = '';
-
-        // // Add to all DS (DB, global), for later use
-        // this.globalVariableService.addDatasource(newData).then(res => {
-        //     this.currentDatasources = this.globalVariableService.currentDatasources.slice()
-
-        //     // Get new dSetID
-        //     // TODO - do better with DB
-        //     let newdSetID: number = 1;
-        //     let dSetIDs: number[] = [];
-        //     this.globalVariableService.datasets.forEach(ds => dSetIDs.push(ds.id));
-        //     newdSetID = Math.max(...dSetIDs) + 1;
-
-        //     // Get list of dSet-ids to make array work easier
-        //     let dsCurrIDs: number[] = [];       // currentDataset IDs
-        //     this.globalVariableService.currentDatasets.forEach(d => dsCurrIDs.push(d.id));
-        //     let newdSet: Dataset = {
-        //         id: newdSetID,
-        //         datasourceID: res.id,
-        //         sourceLocation: 'HTTP',
-        //         url: 'data',
-        //         folderName: '',
-        //         fileName: '',
-        //         data: this.worksheetData,
-        //         dataRaw: this.worksheetData
-        //     };
-
-        //     let dataToAdd: any = {
-        //         id: newdSetID,
-        //         data: this.worksheetData
-        //     };
-
-        //     // Add to All datasets
-        //     if (dSetIDs.indexOf(newdSetID) < 0) {
-        //         // this.globalVariableService.datasets.push(newdSet);
-        //         this.globalVariableService.addDataset(newdSet);
-        //         this.globalVariableService.addData(dataToAdd).then(res => {
-        //             this.globalVariableService.getData(res.id).then(dat => {
-        //                 console.warn('xx ----------')
-        //                 console.warn('xx added data', dat)
-        //             });
-        //         });
-        //         this.globalVariableService.saveLocal('Dataset', newdSet);
-        //     } else {
-        //         // Add to CurrentDatasets
-        //         if (dsCurrIDs.indexOf(newdSetID) < 0) {
-        //             this.globalVariableService.currentDatasets.push(newdSet);
-        //         };
-        //     };
-
-
-        //     console.warn('xx ----------')
-        //     console.warn('xx @end newdSet-datasets-currentDatasets', newdSet, this.globalVariableService.datasets,
-        //     this.globalVariableService.currentDatasets)
-
-        //     // Reset data related to this DS
-        //     console.warn('xx currDS, gv.currDS', this.currentDatasources , this.globalVariableService.currentDatasources)
-        //     this.currentDatasources = this.globalVariableService.currentDatasources.slice();
-
-        //     console.log('done DS:', this.currentDatasources, this.globalVariableService.datasources)
-        // });
-
-        // if (action == 'Saved') {
-        //     this.formDataDirectFileSpreadsheetClosed.emit(null);
-        // } else {
-        //     this.formDataDirectFileSpreadsheetClosed.emit(this.selectedDatasource);
-        // };
 
         // Save changes to the Datasource
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSave', '@Start');
@@ -479,7 +340,7 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
                 id: dataID,
                 data: this.worksheetData
             };
-            console.warn('xx dataID updatedata', dataID, updatedData)
+
             // Add Data, then dataset, then DS
             this.globalVariableService.saveData(updatedData).then(resData => {
 
@@ -487,7 +348,6 @@ export class DataDirectFileSpreadsheetComponent implements OnInit {
                 this.globalVariableService.saveDatasource(this.selectedDatasource).then(
                     resDS => {
                         updatedDataset.datasourceID = this.selectedDatasource.id;
-                        console.warn('xx updatedDataset', updatedDataset)
                         this.globalVariableService.saveDataset(updatedDataset);
                 });
 
