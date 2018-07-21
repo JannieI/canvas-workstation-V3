1d0e6422d2c25c01ca991dd3024acb88c396aa77
JannieI <jimmelman@gmail.com>
2018-07-21 11:45:21 +0200

    Man SQL Ed,v 1

--- src/app/data.managed.sqlEditor.component.ts
+++ src/app/data.managed.sqlEditor.component.ts
@@ -16,12 +16,11 @@ import { GlobalFunctionService } 	  from './global-function.service';
 import { GlobalVariableService }      from './global-variable.service';
 
 // Our Models
-import { DataConnection }             from './models';
-import { DataTable }                  from './models';
-import { DataField }                  from './models';
+import { Dataset }                    from './models';
 import { Datasource }                 from './models';
+import { TributaryServerType }        from './models';
 
- 
+
 @Component({
     selector: 'data-managedSqlEditor',
     templateUrl: './data.managed.sqlEditor.component.html',
@@ -29,6 +28,7 @@ import { Datasource }                 from './models';
 })
 export class DataManagedSQLEditorComponent implements OnInit {
 
+    @Input() editingDS: boolean;
     @Input() selectedDatasource: Datasource;
 
     @Output() formDataManagedSQLEditorClosed: EventEmitter<Datasource> = new EventEmitter();
@@ -40,52 +40,50 @@ export class DataManagedSQLEditorComponent implements OnInit {
 
         // Known ones
         if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
-            this.clickClose('Close');
+            this.clickClose();
             return;
         };
 
     }
 
-    authentication: string = 'UsrPsw';
-    connectionName: string = '';
-    serverType: string = 'MySQL';
-    datasourceName: string = '';
-    description: string = 'Post Trade Data Vault';
-    dataConnections: DataConnection[] = [];
-    dataTables: DataTable[] = [];
-    dataTablesFiltered: DataTable[] = [];
-    dataFields: DataField[] = [];
-    dataFieldsFiltered: DataField[] = [];
-    errorMessage: string = "";
-    selectedFieldRowIndex: number = 0;
-    selectedFields: DataField[] = [];
-    selectedTableRowIndex: number = 0;
-    serverName: string = 'MSSQL54: 8000';
 
-    // connections ->
+    canSave: boolean = false;
+    errorMessage: string = "";
+    fileData: any = [];
+    fileDataFull: any = [];
+    reader = new FileReader();
+    savedMessage: string = '';
+    // selectedFields: string = "BillingAddress,BillingCity,BillingCountry,BillingPostalCode,BillingState,CustomerId,InvoiceDate,InvoiceId,Total";
+    serverTypes: TributaryServerType[];
+    showPreview: boolean = false;
+    spinner: boolean = false;
 
 	constructor(
         private globalFunctionService: GlobalFunctionService,
         private globalVariableService: GlobalVariableService,
-        private router: Router,
 	) {}
 
 	ngOnInit() {
         // Initialise
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
+
+        // Set base info
+        this.serverTypes = this.globalVariableService.serverTypes;
+
         if (this.selectedDatasource == null) {
+            let today: Date = new Date();
             this.selectedDatasource = {
-                id: 0,
-                type: '',
+                id: null,
+                type: 'Server',
                 subType: '',
                 typeVersion: '',
                 name: '',
-                username: '',
-                password: '',
-                description: '...',
-                createdBy: '',
-                createdOn: null,
-                createMethod: 'managedSQLEditor',
+                username: 'ftfhgfzh',
+                password: 'L0Eph9ftbx0yh45aeDtgzsGKBa2ZNhfl',
+                description: '',
+                createdBy: this.globalVariableService.currentUser.userID,
+                createdOn: today,
+                createMethod: 'directSQLEditor',
                 editor: '',
                 dateEdited: null,
                 refreshedBy: '',
@@ -107,125 +105,240 @@ export class DataManagedSQLEditorComponent implements OnInit {
                 connectionID: 0,
                 dataTableID: 0,
                 nrWidgets: 0,
-                databaseName: '',
-                port: '',
-                serverType: '',
-                serverName: '',
-                dataTableName: '',
-                dataSQLStatement: '',
+                databaseName: 'ftfhgfzh',
+                port: '5432',
+                serverType: 'PostgresSQL',
+                serverName: 'pellefant.db.elephantsql.com',
+                dataTableName: 'ftfhgfzh',
+                dataSQLStatement: 'SELECT "InvoiceDate", "BillingCity"  FROM invoices',
                 dataNoSQLStatement: '',
                 dataNeo4jStatement: '',
                 dataGraphQLStatement: '',
-                businessGlossary: '',
+                businessGlossary: 'Obtained using SQL Editor',
                 dataDictionary: ''
+
             };
         };
 
-        this.globalVariableService.getDataConnections().then(dc => {
-            this.globalVariableService.getDataTable().then(dt => {
-                this.globalVariableService.getDataField().then(df => {
-
-                    // Get local Vars
-                    this.dataConnections = dc.slice();
-                    this.dataTables = dt.slice();
-                    this.dataFields = df.slice();
-
-                    // Select the Tables, Fields
-                    if (this.dataConnections.length > 0) {
-                        this.clickConnectionSelect(this.dataConnections[0].connectionName);
-
-                    } else {
-                        this.clickConnectionSelect('');
-                    };
-                });
-            });
-        });
-
-    }
-
-    clickConnectionSelect(ev: any) {
-        // Refresh the Tables and Fields for the selected Connection
-        this.globalFunctionService.printToConsole(this.constructor.name,'clickConnectionSelect', '@Start');
-
-        console.warn('xx ev', ev, this.connectionName)
-
-        // Fill list of Tables for first Connection
-        if (this.connectionName != '') {
-            this.filterTables(this.connectionName);
-        } else {
-            this.filterTables('');
-        };
-
-        // Fill list of Fields for first Table
-        if (this.dataTablesFiltered.length > 0) {
-            this.filterFields(this.dataTablesFiltered[0].id);
-        } else {
-            this.filterFields(-1);
-        };
-
-    }
-
-    filterTables(connectNameToFilter: string) {
-        // Filter Tables on Selected Connection
-        this.globalFunctionService.printToConsole(this.constructor.name,'filterTables', '@Start');
-
-        let connectionIndex: number = this.dataConnections.findIndex(dt =>
-            dt.connectionName == connectNameToFilter
-        );
-        let connectionID: number = -1;
-        if (connectionIndex >= 0) {
-            connectionID = this.dataConnections[connectionIndex].id;
-        };
-
-        console.warn('xx conn', connectionID, connectNameToFilter, connectionIndex)
-        this.dataTablesFiltered = this.dataTables.filter(dt => {
-            if (dt.connectionID == connectionID) {
-                return dt;
-            };
-        });
-
-    }
-    
-    filterFields(tableID: number) {
-        // Filter Fields on Selected Connection
-        this.globalFunctionService.printToConsole(this.constructor.name,'filterFields', '@Start');
-
-        this.dataFieldsFiltered = this.dataFields.filter(df => {
-            if (df.tableID == tableID) {
-                return df;
-            };
-        });
 
     }
 
     clickGo() {
-        // Clicked Go: execute SQL typed in, and return results and errors
-        this.globalFunctionService.printToConsole(this.constructor.name,'clickGo', '@Start');
+        // Load the File content
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickGo',           '@Start');
+
+        // Reset
+        this.errorMessage = '';
+        this.showPreview = false;
+        this.canSave = false;
+
+        // Show user
+        this.spinner = true;
+
+        // Get drivers
+        let driver: string = this.serverTypes
+            .filter(styp => styp.serverType == this.selectedDatasource.serverType)
+            .map(styp => styp.driverName)[0];
+
+        let specificationInspect: any = {
+            "source": {
+                "inspector": "tributary.inspectors.sql:SqlInspector",
+                "specification": {
+                    "drivername": driver,
+                    "username": this.selectedDatasource.username,
+                    "password": this.selectedDatasource.password,
+                    "database": this.selectedDatasource.databaseName,
+                    "host": this.selectedDatasource.serverName,
+                    "port": +this.selectedDatasource.port
+                }
+            }
+        };       
+        // Call Tributary
+        this.globalVariableService.getTributaryInspect(specificationInspect).then(res => {
+            console.warn('xx res I', res)
+
+            // Set up specification
+            this.selectedDatasource.dataSQLStatement = this.selectedDatasource.dataSQLStatement.trim();
+            let specificationConnect: any = {
+                "source": {
+                    "connector": "tributary.connectors.sql:SqlConnector",
+                    "specification": {
+                        "drivername": driver,
+                        "username": this.selectedDatasource.username,
+                        "password": this.selectedDatasource.password,
+                        "database": this.selectedDatasource.databaseName,
+                        "host": this.selectedDatasource.serverName,
+                        "port": +this.selectedDatasource.port,
+                        "query": this.selectedDatasource.dataSQLStatement
+                    }
+                }
+            };
+
+            this.globalVariableService.getTributaryData(specificationConnect).then(res => {
+
+                // Fill the data
+                this.fileData = res.slice(0,10);
+                this.fileDataFull = res;
+                // this.selectedDatasource.dataFields = this.selectedFields.split(",");
+
+                // Construct a list of field name / column headings from the data
+                this.selectedDatasource.dataFields = [];
+
+                if (res.length > 0) {
+                    console.warn('xx res[0]', res[0])
+                    for(var key in res[0]) {
+                        console.warn('xx key', key)
+                        this.selectedDatasource.dataFields.push(key);
+                    }
+                };
+                // Show the results
+                this.showPreview = true;
+                this.spinner = false;
+
+                // Can Add now
+                this.canSave = true;
+
+            })
+            .catch(err => {
+                this.spinner = false;
+                this.errorMessage = 'Error connecting to server: check login or permissions'
+                    + err.message;
+            });
+        })
+        .catch(err => {
+            console.warn('xx err', err)
+            this.spinner = false;
+            this.errorMessage = 'Error connecting to server: check login or permissions'
+                + err.message;
+        });
 
     }
 
-    clickClose(action: string) {
-        //
+    clickClose() {
+        // Close the form, nothing saved
         this.globalFunctionService.printToConsole(this.constructor.name,'clickClose', '@Start');
 
         this.formDataManagedSQLEditorClosed.emit(null);
 
     }
- 
-    clickSave(action: string) {
-        // Close the form, and open Transformations form
+
+    clickAdd(action: string) {
+        // Add the DS, with data, to the DB
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickAdd', '@Start');
+
+        // Reset
+        this.errorMessage = '';
+        this.savedMessage = '';
+
+        // Save changes to the Datasource
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSave', '@Start');
 
-        console.warn('xx END sel ds', this.selectedDatasource)
+        // Validation
+        if (this.selectedDatasource.name == ''  ||  this.selectedDatasource.name == null) {
+            this.errorMessage = 'The name is compulsory';
+            return;
+        };
+        if (this.selectedDatasource.description == ''  ||  this.selectedDatasource.description == null) {
+            this.errorMessage = 'The description is compulsory';
+            return;
+        };
+
+        // Construct DS and add to DB
+        if (this.editingDS) {
+            let today: Date = new Date();
+            this.selectedDatasource.editor = this.globalVariableService.currentUser.userID;
+            this.selectedDatasource.dateEdited = today;
+
+            // Save DS to DB, but create a new dSet and new data records.
+            let ds: number[] = [];
+            let dSetID: number = 1;
+            for (var i = 0; i < this.globalVariableService.datasets.length; i++) {
+                if(this.globalVariableService.datasets[i].datasourceID ==
+                    this.selectedDatasource.id) {
+                    ds.push(this.globalVariableService.datasets[i].id)
+                };
+            };
+            if (ds.length > 0) {
+                dSetID = Math.max(...ds);
+            };
+            let datasetIndex: number = this.globalVariableService.datasets.findIndex(dSet => {
+                if (dSet.id == dSetID) {
+                    return dSet;
+                };
+            });
+            let updatedDataset: Dataset = this.globalVariableService.datasets[datasetIndex];
+
+            let dataID: number = -1;
+            let dataIndex: number = updatedDataset.url.indexOf('/');
+            if (dataIndex >= 0) {
+                dataID = +updatedDataset.url.substring(dataIndex + 1);
+            } else {
+                alert('Error in save Web - url has no / character');
+                return;
+            };
+            let updatedData: any = {
+                id: dataID,
+                data: this.fileDataFull
+            };
+
+            // Add Data, then dataset, then DS
+            this.globalVariableService.saveData(updatedData).then(resData => {
+
+                updatedDataset.url = 'data/' + dataID;
+                this.globalVariableService.saveDatasource(this.selectedDatasource).then(
+                    resDS => {
+                        updatedDataset.datasourceID = this.selectedDatasource.id;
+                        this.globalVariableService.saveDataset(updatedDataset);
+                });
+
+                // Indicate to the user
+                this.canSave = false;
+                this.savedMessage = 'Datasource updated';
+            });
+
+        } else {
+            // Add new one
+            let newdDataset: Dataset = {
+                id: null,
+                datasourceID: null,
+                sourceLocation: 'HTTP',
+                url: 'data',
+                folderName: '',
+                fileName: '',
+                data: null,
+                dataRaw: null
+            };
+            let newData: any = {
+                id: null,
+                data: this.fileDataFull
+            };
+
+            // Add Data, then dataset, then DS
+            this.globalVariableService.addData(newData).then(resData => {
+
+                newdDataset.url = 'data/' + resData.id.toString();
+                this.globalVariableService.addDatasource(this.selectedDatasource).then(resDS => {
+                    newdDataset.datasourceID = resDS.id;
+                    this.globalVariableService.addDataset(newdDataset);
+
+                });
+
+                // Indicate to the user
+                this.canSave = false;
+                this.savedMessage = 'Datasource created';
+            });
+        };
+
+        // Close form and open Transitions if requested
         if (action == 'Saved') {
             this.formDataManagedSQLEditorClosed.emit(null);
 
         } else {
             this.formDataManagedSQLEditorClosed.emit(this.selectedDatasource);
 
-        }
-
+        };
     }
+
 }
 
 
