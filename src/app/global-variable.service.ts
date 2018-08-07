0d339e944854a69a1fe68f6da438ae554c2618f5
JannieI <jimmelman@gmail.com>
2018-08-07 10:18:33 +0200

    Add dateAdd

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -8959,4 +8959,33 @@ export class GlobalVariableService {
         return tributarySource;
     };
 
+    dateAdd(date, interval, units) {
+        // Adds an element to a data, similar to ADDDATE SQL-style
+        //  - date  Date to start with
+        //  - interval  One of: year, quarter, month, week, day, hour, minute, second
+        //  - units  Number of units of the given interval to add.
+        //  Example: dateAdd(new Date(), 'minute', 30)  //returns 30 minutes from now
+        // Returns: Amended Date
+        console.log('%c    Global-Variables getTributaryDirectDBSchema ...',
+        "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", 
+        {date}, {interval}, {units});
+
+        // Get the original
+        var ret = new Date(date); //don't change original date
+
+        var checkRollover = function() { if(ret.getDate() != date.getDate()) ret.setDate(0);};
+        switch(interval.toLowerCase()) {
+            case 'year'   :  ret.setFullYear(ret.getFullYear() + units); checkRollover();  break;
+            case 'quarter':  ret.setMonth(ret.getMonth() + 3*units); checkRollover();  break;
+            case 'month'  :  ret.setMonth(ret.getMonth() + units); checkRollover();  break;
+            case 'week'   :  ret.setDate(ret.getDate() + 7*units);  break;
+            case 'day'    :  ret.setDate(ret.getDate() + units);  break;
+            case 'hour'   :  ret.setTime(ret.getTime() + units*3600000);  break;
+            case 'minute' :  ret.setTime(ret.getTime() + units*60000);  break;
+            case 'second' :  ret.setTime(ret.getTime() + units*1000);  break;
+            default       :  ret = undefined;  break;
+        };
+        return ret;
+    }
+
 }
