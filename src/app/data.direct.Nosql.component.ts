97c4e46cbb2f11ad1b72a23bc0cee88aa70dabc3
JannieI <jimmelman@gmail.com>
2018-08-08 10:01:23 +0200

    Amend Dir NoSQL

--- src/app/data.direct.Nosql.component.ts
+++ src/app/data.direct.Nosql.component.ts
@@ -100,12 +100,9 @@ export class DataDirectNoSQLComponent implements OnInit {
                 dataFieldLengths: [0],
                 parameters: '',
                 cacheResultsOnServer: true,
-                cacheServerStorageID: null,
                 unRefreshable: true,
                 nrCacheCopies: 999,
                 cacheResultsLocal: false,
-                cacheLocalStorageID: null,
-                isLocalDirty: null,
                 oldnessMaxPeriodInterval: '',
                 oldnessMaxPeriodUnits: 0,
                 oldnessRelatedDate: '',
@@ -324,6 +321,9 @@ export class DataDirectNoSQLComponent implements OnInit {
                 url: 'data',
                 folderName: '',
                 fileName: '',
+                cacheServerStorageID: null,
+                cacheLocalStorageID: null,
+                isLocalDirty: null,
                 data: this.fileDataFull,
                 dataRaw: this.fileDataFull
             };
