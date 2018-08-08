7324a6607c2edaaff48817c5f0977404fe6ae57b
JannieI <jimmelman@gmail.com>
2018-08-08 10:04:10 +0200

    Amend Man NoSQL

--- src/app/data.managed.noSqlEditor.component.ts
+++ src/app/data.managed.noSqlEditor.component.ts
@@ -104,12 +104,9 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
                 dataFieldLengths: [0],
                 parameters: '',
                 cacheResultsOnServer: true,
-                cacheServerStorageID: null,
                 unRefreshable: true,
                 nrCacheCopies: 999,
                 cacheResultsLocal: false,
-                cacheLocalStorageID: null,
-                isLocalDirty: null,
                 oldnessMaxPeriodInterval: '',
                 oldnessMaxPeriodUnits: 0,
                 oldnessRelatedDate: '',
@@ -317,6 +314,9 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
                 url: 'data',
                 folderName: '',
                 fileName: '',
+                cacheServerStorageID: null,
+                cacheLocalStorageID: null,
+                isLocalDirty: null,
                 data: this.fileDataFull,
                 dataRaw: this.fileDataFull
             };
