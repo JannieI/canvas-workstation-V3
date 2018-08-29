f6a0aeddf8515e92a94bd3b39a635d961e3b65dc
JannieI <jimmelman@gmail.com>
2018-08-29 13:38:13 +0200

    clickToggleGrantAccess

--- src/app/dashboard.share.component.ts
+++ src/app/dashboard.share.component.ts
@@ -310,6 +310,10 @@ export class DashboardShareComponent implements OnInit {
             if (this.dashboardPermissions[i].id == id) {
                 this.dashboardPermissions[i].canGrantAccess = ! this.dashboardPermissions[i].canGrantAccess;
                 index = i;
+
+                // Update Grantor and -On
+                this.dashboardPermissions[i].grantor = this.globalVariableService.currentUser.userID;
+                this.dashboardPermissions[i].grantedOn = new Date();
             };
         };
 
