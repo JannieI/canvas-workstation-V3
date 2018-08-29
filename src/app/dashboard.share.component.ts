e0a740c8664a1e6eb0db4f282b8b5442097a02fc
JannieI <jimmelman@gmail.com>
2018-08-29 13:52:09 +0200

    clickToggleSave

--- src/app/dashboard.share.component.ts
+++ src/app/dashboard.share.component.ts
@@ -248,6 +248,12 @@ export class DashboardShareComponent implements OnInit {
         // User dblclicked Save - so toggle it
         this.globalFunctionService.printToConsole(this.constructor.name,'clickToggleSave', '@Start');
 
+        if (!this.globalVariableService.dashboardPermissionCheck(
+            this.selectedDashboard.id, 'cangrantaccess') ) {
+                this.errorMessage = 'You cannot Grant access to others';
+                return;
+        };
+
         let index: number = -1;
         for(var i = 0; i < this.dashboardPermissions.length; i++) {
             if (this.dashboardPermissions[i].id == id) {
