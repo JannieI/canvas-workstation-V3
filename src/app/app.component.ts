baa9ed11d274c4b7b0ab8411095af26a78400481
JannieI <jimmelman@gmail.com>
2018-09-16 13:37:23 +0200

    clickMenuDataDirectWeb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3737,6 +3737,20 @@ console.warn('xx APP start', this.globalVariableService.currentWidgets)
         // Open DATA form for a DS that comes from a Web page.
         this.globalFunctionService.printToConsole(this.constructor.name,'clickMenuDataDirectWeb', '@Start');
 
+        // Permissions
+        if (!this.globalVariableService.currentUser.datasourceCanCreateRole
+            &&
+            !this.globalVariableService.currentUser.isAdministrator) {
+            this.showMessage(
+                'You cannot add a new Datasource (role must be added to your user)',
+                'StatusBar',
+                'Warning',
+                3000,
+                ''
+            );
+            return;
+        };
+
         this.menuOptionClickPreAction();
 
         this.editingDS = false;
