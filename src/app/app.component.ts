6e9be9368a1e3d0f718f68017d7ac3e235648b7a
JannieI <jimmelman@gmail.com>
2018-09-17 06:50:51 +0200

    clickMenuDataManagedNeo4jEditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3502,8 +3502,22 @@ console.warn('xx APP start', this.globalVariableService.currentWidgets)
     }
 
     clickMenuDataManagedNeo4jEditor() {
-        // Ne04j Editor
+        // Neo4j Editor
         this.globalFunctionService.printToConsole(this.constructor.name,'clickMenuDataManagedNeo4jEditor', '@Start');
+
+        // Permissions
+        if (!this.globalVariableService.currentUser.datasourceCanCreateRole
+            &&
+            !this.globalVariableService.currentUser.isAdministrator) {
+            this.showMessage(
+                'You cannot add a new Datasource (role must be added to your user)',
+                'StatusBar',
+                'Warning',
+                3000,
+                ''
+            );
+            return;
+        };
 
         this.menuOptionClickPreAction();
 
