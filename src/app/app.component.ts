44b72461669545b7aac188634e3515a360f8b428
JannieI <jimmelman@gmail.com>
2018-09-17 06:49:44 +0200

    clickMenuDataManagedSQLEditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3436,6 +3436,20 @@ console.warn('xx APP start', this.globalVariableService.currentWidgets)
         // SQL Editor
         this.globalFunctionService.printToConsole(this.constructor.name,'clickMenuDataManagedSQLEditor', '@Start');
 
+        // Permissions
+        if (!this.globalVariableService.currentUser.datasourceCanCreateRole
+            &&
+            !this.globalVariableService.currentUser.isAdministrator) {
+            this.showMessage(
+                'You cannot add a new Datasource (role must be added to your user)',
+                'StatusBar',
+                'Warning',
+                3000,
+                ''
+            );
+            return;
+        };
+
         this.menuOptionClickPreAction();
 
         this.showModalDataManagedSQLEditor = true;
