a5c5a5ba192a1b348061ecca4b3619707df88673
JannieI <jimmelman@gmail.com>
2018-11-02 11:27:40 +0200

    getTributaryData

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -12704,7 +12704,6 @@ console.warn('xx ds perm', dp);
         };
 
         let pathUrl: string = this.ENVCanvasEZALServerUrl + 'canvas/enqueue/';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
         this.filePath = './assets/data.dashboards.json';
 
         return new Promise<any>((resolve, reject) => {
