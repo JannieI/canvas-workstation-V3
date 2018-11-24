9bc9962a36f3b3e3922f68ea9995ac3dba0b11cb
JannieI <jimmelman@gmail.com>
2018-11-24 10:20:07 +0200

    clickClearProjectionFieldLongitude

--- src/app/widget.editor.componentNEW.ts
+++ src/app/widget.editor.componentNEW.ts
@@ -2632,8 +2632,13 @@ export interface dataSchemaInterface {
         this.projectionFieldLongitude = dragFieldMessage;
         this.localWidget.graphLayers[this.currentGraphLayer - 1].graphProjectionFieldLatitude = '';
 
-        // Hide the panel with properties
-        this.showFieldProjectionProperties = false;
+
+        // Hide the panel with properties if not fields selected
+        if (this.projectionFieldLatitude == dragFieldMessage  
+            &&  
+            this.projectionFieldLongitude == dragFieldMessage) {
+            this.showFieldProjectionProperties = false;
+        };
 
     }
 
