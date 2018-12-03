04e0faeb3bd1b696b88f5e9d1a3a21fe60f1b1e8
JannieI <jimmelman@gmail.com>
2018-12-03 07:50:31 +0200

    ENV file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,24 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
+// The Eazl/Canvas server indicates where Canvas will get data from.  Each of these
+// has ENV parameters to indicate where their data is, etc
 export const environment = {
   production: false,
   
+  // Eazl Server
   ENVCanvasEazlServerUrl: "https://eazl-rest.xyz/eazl/api/",
-  ENVCanvasDatabaseUseLocal: true,
-  ENVCanvasDatabaseServerUrl: "canvasServer",
-  ENVCanvasDatabaseLocalUrlS1: "http://localhost:3001/",
-  ENVCanvasDatabaseLocalUrlS2: "http://localhost:3000/",
-  ENVCanvasDatabaseLocalUrlS3: "http://localhost:3002/",
-  ENVCanvasDatabaseLocalUrlS4: "http://localhost:3005/",
-  ENVCanvasDatabaseLocalUrlS5: "http://localhost:3006/"
+
+  // Canvas Server Cloud
+  ENVCanvasServerCloudUrl: "http://localhost:8000",
+
+  // Canvas Server Local
+  ENVCanvasServerLocalUrl: "http://localhost:8000",
+
+  // JSON-SERVER Urls
+  ENVCanvasDatabaseLocalUrlS1: "http://localhost:3001",
+  ENVCanvasDatabaseLocalUrlS2: "http://localhost:3000",
+  ENVCanvasDatabaseLocalUrlS3: "http://localhost:3002",
+  ENVCanvasDatabaseLocalUrlS4: "http://localhost:3005",
+  ENVCanvasDatabaseLocalUrlS5: "http://localhost:3006"
 };
