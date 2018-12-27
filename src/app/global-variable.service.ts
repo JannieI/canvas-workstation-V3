196221016e192e13e4dba1036cb811b40f437807
JannieI <jimmelman@gmail.com>
2018-12-27 14:52:44 +0200

    deleteBackgroundColor

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -8699,7 +8699,6 @@ export class GlobalVariableService {
         });
     }
 
-
     saveBackgroundColor(data: CSScolor): Promise<string> {
         // Description: Saves BackgroundColor
         // Returns: 'Saved' or error message
@@ -8759,18 +8758,19 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'canvasBackgroundcolors';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.BackgroundColor.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
+            let pathUrl: string = 'canvasBackgroundcolors';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+                    };
 
                     // This is a different case: BackgroundColors is an
                     // Observable, and will be refreshed with a .next by the calling
@@ -8798,7 +8798,7 @@ export class GlobalVariableService {
                         console.log('Error deleteBackgroundColor FAILED', {err});
                     };
 
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
