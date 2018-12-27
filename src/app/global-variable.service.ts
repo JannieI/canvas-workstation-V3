a3e50e376a508cd8081e356a5009bd906e178e75
JannieI <jimmelman@gmail.com>
2018-12-27 20:18:02 +0200

    addContainerStyle

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -10108,35 +10108,36 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {data});
         };
 
-        let pathUrl: string = 'containerStyles';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.ContainerStyles.json';
-
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.post(finalUrl, data, {headers})
-                .subscribe(
-                    res => {
+
+            let pathUrl: string = 'containerStyles';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.post<CanvasHttpResponse>(finalUrl, data, {headers})
+            .subscribe(
+                res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+                    };
 
                         // Update Global vars to make sure they remain in sync
-                        this.containerStyles.push(JSON.parse(JSON.stringify(res)));
+                        this.containerStyles.push(JSON.parse(JSON.stringify(res.data)));
 
                         if (this.sessionDebugging) {
                             console.log('addContainerStyle ADDED', {res})
                         };
 
-                        resolve(res);
+                        resolve(res.data);
                     },
                     err => {
                         if (this.sessionDebugging) {
                             console.log('Error addContainerStyle FAILED', {err});
                         };
 
-                        reject(err);
+                        reject(err.message);
                     }
                 )
         });
