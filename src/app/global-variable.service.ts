9aece42d53d659aa349fd1b038396cfb1b4821ab
JannieI <jimmelman@gmail.com>
2018-12-28 20:47:31 +0200

    deleteDashboardSubscription

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -7675,18 +7675,19 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'dashboardSubscriptions';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.dashboardSubscriptions.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
-            .subscribe(
+            let pathUrl: string = 'dashboardSubscriptions';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers}).subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
 
                     this.dashboardSubscriptions = this.dashboardSubscriptions.
                         filter(sub => sub.id != id);
@@ -7704,7 +7705,7 @@ export class GlobalVariableService {
                     if (this.sessionDebugging) {
                         console.log('Error deleteDashboardSubscription FAILED', {err});
                     };
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
