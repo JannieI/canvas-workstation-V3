81aab955678b249b8d5bfc0dd1d2300148d2843f
JannieI <jimmelman@gmail.com>
2018-12-28 20:46:04 +0200

    saveDashboardSubscription

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -7624,18 +7624,25 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {data});
         };
 
-        let pathUrl: string = 'dashboardSubscriptions';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.dashboardSubscriptions.json';
-
         return new Promise<string>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.put(finalUrl + '/' + data.id, data, {headers})
+            let pathUrl: string = 'dashboardSubscriptions';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+
+            // Omit _id (immutable in Mongo)
+            const copyData = { ...data };
+            delete copyData._id;
+
+            this.http.put<CanvasHttpResponse>(finalUrl + '?id=' + copyData.id, copyData, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
 
                     // Replace local
                     let localIndex: number = this.dashboardSubscriptions.findIndex(d =>
@@ -7654,7 +7661,7 @@ export class GlobalVariableService {
                     if (this.sessionDebugging) {
                         console.log('Error saveDashboardSubscription FAILED', {err});
                     };
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
