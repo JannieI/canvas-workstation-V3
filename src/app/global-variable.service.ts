4f0e5c86730fc3c8c55c980b849d2f3294380199
JannieI <jimmelman@gmail.com>
2018-12-29 20:23:02 +0200

    Add deleteCanvasGroup

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -5941,6 +5941,50 @@ export class GlobalVariableService {
         });
     }
 
+    deleteCanvasGroup(id: number, widgetID: number = null): Promise<string> {
+        // Description: Deletes a canvasGroups
+        // Returns: 'Deleted' or error message
+        if (this.sessionDebugging) {
+            console.log('%c    Global-Variables deleteCanvasGroup ...',
+                "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
+        };
+
+        return new Promise<any>((resolve, reject) => {
+
+            const headers = new HttpHeaders()
+                .set("Content-Type", "application/json");
+
+            let pathUrl: string = 'canvasGroups';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
+            .subscribe(
+                res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+                        return;
+                    };
+
+                    this.canvasGroups = this.canvasGroups.filter(
+                        grp => grp.id != id
+                    );
+
+                    if (this.sessionDebugging) {
+                        console.log('deleteCanvasGroup DELETED id: ', {id})
+                    };
+
+                    resolve('Deleted');
+                },
+                err => {
+                    if (this.sessionDebugging) {
+                        console.log('Error deleteCanvasGroup FAILED', {err});
+                    };
+
+                    reject(err.message);
+                }
+            )
+        });
+    }
+
     getWidgetGraphs(): Promise<WidgetGraph[]> {
         // Description: Gets all G
         // Returns: this.WidgetGraphs array, unless:
