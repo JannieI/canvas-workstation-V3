22114a3b4707dc53fe8ea0869158fc6fb6e73780
JannieI <jimmelman@gmail.com>
2018-12-29 20:21:29 +0200

    Add saveCanvasGroup

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -5888,6 +5888,59 @@ export class GlobalVariableService {
         });
     }
 
+    saveCanvasGroup(data: CanvasGroup): Promise<string> {
+        // Description: Saves CanvasGroup
+        // Returns: 'Saved' or error message
+        if (this.sessionDebugging) {
+            console.log('%c    Global-Variables saveCanvasGroup ...',
+                "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {data});
+        };
+
+        return new Promise<string>((resolve, reject) => {
+
+            const headers = new HttpHeaders()
+                .set("Content-Type", "application/json");
+
+            let pathUrl: string = 'canvasGroups';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+
+            // Omit _id (immutable in Mongo)
+            const copyData = { ...data };
+            delete copyData._id;
+
+            this.http.put<CanvasHttpResponse>(finalUrl + '?id=' + copyData.id, copyData, {headers})
+            .subscribe(
+                res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
+            // this.http.put(finalUrl + '?id=' + data.id, data, {headers}).subscribe(res => {
+
+                // Replace local
+                let localIndex: number = this.canvasGroups.findIndex(u =>
+                    u.id == data.id
+                );
+                if (localIndex >= 0) {
+                    this.canvasGroups[localIndex] = data;
+                };
+
+                if (this.sessionDebugging) {
+                    console.log('saveCanvasGroup SAVED', res.data)
+                };
+
+                resolve('Saved');
+            },
+            err => {
+                if (this.sessionDebugging) {
+                    console.log('Error saveCanvasGroup FAILED', {err});
+                };
+
+                reject(err.message);
+            });
+        });
+    }
+
     getWidgetGraphs(): Promise<WidgetGraph[]> {
         // Description: Gets all G
         // Returns: this.WidgetGraphs array, unless:
