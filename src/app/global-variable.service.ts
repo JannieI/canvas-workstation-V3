d81a0c04f5566271bdf02d767208bc505ea565d2
JannieI <jimmelman@gmail.com>
2018-12-29 16:03:36 +0200

    deleteDataConnection

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -3783,18 +3783,20 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'DataConnections';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.DataConnections.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
+            let pathUrl: string = 'DataConnections';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
 
                     this.dataConnections = this.dataConnections.filter(
                         dsp => dsp.id != id
@@ -3811,7 +3813,7 @@ export class GlobalVariableService {
                         console.log('Error deleteDataConnection FAILED', {err});
                     };
 
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
