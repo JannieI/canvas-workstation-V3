2ecc1567754639671cd39fc7e7fee73c88de2574
JannieI <jimmelman@gmail.com>
2018-12-29 09:28:02 +0200

    deleteDashboardTab

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -3184,18 +3184,20 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'dashboardTabs';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.dashboardTabs.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
+            let pathUrl: string = 'dashboardTabs';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
 
                     this.dashboardTabs = this.dashboardTabs.filter(
                         dsp => dsp.id != id
@@ -3215,7 +3217,7 @@ export class GlobalVariableService {
                     if (this.sessionDebugging) {
                         console.log('Error deleteDashboardTab FAILED', {err});
                     };
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
