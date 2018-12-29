78cb394082f8ae59aef22bd9731280ed52f94671
JannieI <jimmelman@gmail.com>
2018-12-29 16:07:33 +0200

    deleteDatasourceTransformation

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -3972,18 +3972,20 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'datasourceTransformations';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.datasourceTransformations.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
+            let pathUrl: string = 'datasourceTransformations';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
 
                     this.datasourceTransformations = this.datasourceTransformations.filter(
                         dsp => dsp.id != id
@@ -4000,7 +4002,7 @@ export class GlobalVariableService {
                         console.log('Error deleteDatasourceTransformation FAILED', {err});
                     };
 
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
