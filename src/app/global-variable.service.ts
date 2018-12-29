186cfb6cc54d31f601d71bd215403eea42c5c4aa
JannieI <jimmelman@gmail.com>
2018-12-29 15:34:19 +0200

    deleteWidgetLayout

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -10806,18 +10806,21 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'widgetLayouts';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.WidgetLayout.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
+            let pathUrl: string = 'widgetLayouts';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
+
                     this.widgetLayouts = this.widgetLayouts.filter(wl => wl.id != id);
 
                     // TODO - do this better in DB as we are checking local Array length
@@ -10838,7 +10841,7 @@ export class GlobalVariableService {
                         console.log('Error deleteWidgetLayout FAILED', {err});
                     };
 
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
