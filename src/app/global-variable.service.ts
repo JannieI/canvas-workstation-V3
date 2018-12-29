8c5d02d96ddcab6487160f02f7986a27423c4780
JannieI <jimmelman@gmail.com>
2018-12-29 14:33:51 +0200

    deleteDataQualityIssue

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -6920,18 +6920,20 @@ export class GlobalVariableService {
                 "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
         };
 
-        let pathUrl: string = 'dataQualityIssues';
-        let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-        this.filePath = './assets/data.dataQualityIssues.json';
-
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
                 .set("Content-Type", "application/json");
 
-            this.http.delete(finalUrl + '/' + id, {headers})
+            let pathUrl: string = 'dataQualityIssues';
+            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
+            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
             .subscribe(
                 res => {
+                    if(res.statusCode != 'success') {
+                        reject(res.message);
+						return;
+                    };
 
                     this.dataQualityIssues = this.dataQualityIssues.filter(
                         dsp => dsp.id != id
@@ -6950,7 +6952,7 @@ export class GlobalVariableService {
                     if (this.sessionDebugging) {
                         console.log('Error deleteDataQualityIssue FAILED', {err});
                     };
-                    reject(err);
+                    reject(err.message);
                 }
             )
         });
