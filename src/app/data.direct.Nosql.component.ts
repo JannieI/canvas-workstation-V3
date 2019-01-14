a3291a6101900ce19f3e8da15cfc7ef0b03948c7
JannieI <jimmelman@gmail.com>
2019-01-14 09:13:50 +0200

    Direct NoSQL

--- src/app/data.direct.Nosql.component.ts
+++ src/app/data.direct.Nosql.component.ts
@@ -107,6 +107,7 @@ export class DataDirectNoSQLComponent implements OnInit {
                 datasourceFilterForThisDashboard: false,
                 accessType: '',
                 cacheResultsOnServer: true,
+                serverExpiryDateTime: null,
                 unRefreshable: true,
                 nrCacheCopies: 999,
                 cacheResultsLocal: false,
