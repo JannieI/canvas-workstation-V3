15d34b990861beb533de2f59a15b8955ed3097c2
JannieI <jimmelman@gmail.com>
2019-01-18 19:02:32 +0200

    Dir GSheet

--- src/app/data.direct.googleSheets.component.ts
+++ src/app/data.direct.googleSheets.component.ts
@@ -291,7 +291,24 @@ export class DataDirectGoogleSheetsComponent implements OnInit {
                 dataGraphQLStatement: '',
                 dataOverlaySpecification: '',
                 nrWidgets: null,
-                datasourceCombinationSpec: null
+                datasourceCombinationSpec: null,
+                rowLimitFromSource: 0,
+                timeoutLimitSeconds: 0,
+                endLineNr: 0,
+                startColumnNr: 1,
+                endColumnNr: 0,
+                encoding: 'Ascii',
+                serviceUrl: '',
+                serviceParams: '',
+                serviceQueryParams: '',
+                serviceHeaders: '',
+                sourceIsAccessable: true,
+                queryParameters: '',
+                metaDataField: [],
+                transformations: [],
+                dataErrorMessage: '',
+                nrRecordsReturned: 0,
+                sourceLocation: ''
             };
 
             let newdDataset: Dataset = {
