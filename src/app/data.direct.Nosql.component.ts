530ab9e5388ac961e2ceaa5a6b5103eeaaf5e75e
JannieI <jimmelman@gmail.com>
2019-01-18 19:02:17 +0200

    Dir NoSQL

--- src/app/data.direct.Nosql.component.ts
+++ src/app/data.direct.Nosql.component.ts
@@ -139,7 +139,24 @@ export class DataDirectNoSQLComponent implements OnInit {
                 dataOverlaySpecification: '',
                 businessGlossary: 'Obtained using NoSQL Editor',
                 dataDictionary: '',
-                datasourceCombinationSpec: null
+                datasourceCombinationSpec: null,
+                rowLimitFromSource: 0,
+                timeoutLimitSeconds: 0,
+                endLineNr: 0,
+                startColumnNr: 1,
+                endColumnNr: 0,
+                encoding: 'Ascii',
+                serviceUrl: '',
+                serviceParams: '',
+                serviceQueryParams: '',
+                serviceHeaders: '',
+                sourceIsAccessable: true,
+                queryParameters: '',
+                metaDataField: [],
+                transformations: [],
+                dataErrorMessage: '',
+                nrRecordsReturned: 0,
+                sourceLocation: ''
 
             };
         };
