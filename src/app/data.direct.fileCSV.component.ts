8c3417a25fd09286e213a2a0b1341042fed1f04a
JannieI <jimmelman@gmail.com>
2019-02-01 18:11:24 +0200

    Add Enter event

--- src/app/data.direct.fileCSV.component.ts
+++ src/app/data.direct.fileCSV.component.ts
@@ -41,6 +41,16 @@ export class DataDirectFileCSVComponent implements OnInit {
             this.clickClose('Close');
             return;
         };
+        if ( 
+            (event.code == 'Enter'  ||  event.code == 'NumpadEnter')
+            &&  
+            (!event.ctrlKey)  
+            &&  
+            (!event.shiftKey) 
+           ) {
+            this.clickAdd();
+            return;
+        };
 
     }
 
