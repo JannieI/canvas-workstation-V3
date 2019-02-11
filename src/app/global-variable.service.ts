50219d0684dde032d393cc8cd6571e7e32bc69d6
JannieI <jimmelman@gmail.com>
2019-02-11 18:59:03 +0200

    Depr deleteDataQualityIssue

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -7298,49 +7298,6 @@ export class GlobalVariableService {
         };
     }
 
-    deleteDataQualityIssue(id: number): Promise<string> {
-        // Description: Deletes a DataQualityIssues
-        // Returns: 'Deleted' or error message
-        if (this.sessionDebugging) {
-            console.log('%c    Global-Variables deleteDataQualityIssue ...',
-                "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {id});
-        };
-
-        return new Promise<any>((resolve, reject) => {
-
-            const headers = new HttpHeaders()
-                .set("Content-Type", "application/json");
-
-            let pathUrl: string = 'dataQualityIssues';
-            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-            this.http.delete<CanvasHttpResponse>(finalUrl + '?id=' + id, {headers})
-            .subscribe(
-                res => {
-                    if(res.statusCode != 'success') {
-                        reject(res.message);
-						return;
-                    };
-
-                    this.dataQualityIssues = this.dataQualityIssues.filter(
-                        dsp => dsp.id != id
-                    );
-
-                    if (this.sessionDebugging) {
-                        console.log('deleteDataQualityIssue DELETED id: ', {id})
-                    };
-
-                    resolve('Deleted');
-                },
-                err => {
-                    if (this.sessionDebugging) {
-                        console.log('Error deleteDataQualityIssue FAILED', {err});
-                    };
-                    reject(err.message);
-                }
-            )
-        });
-    }
-
     getDataOwnerships(): Promise<DataOwnership[]> {
         // Description: Gets all dQual
         // Returns: this.DataOwnerships array, unless:
