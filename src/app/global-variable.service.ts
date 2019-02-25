cf0623271a59b9805796fb5f9bfcebdd3296d6e6
JannieI <jimmelman@gmail.com>
2019-02-25 16:28:19 +0200

    Depr addCanvasGroup

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -5275,50 +5275,6 @@ export class GlobalVariableService {
         });
     }
 
-    addCanvasGroup(data: CanvasGroup): Promise<any> {
-        // Description: Adds a new canvasGroup
-        // Returns: Added Data or error message
-        if (this.sessionDebugging) {
-            console.log('%c    Global-Variables addCanvasGroup ...',
-                "color: black; background: rgba(104, 25, 25, 0.4); font-size: 10px", {data});
-        };
-
-        return new Promise<any>((resolve, reject) => {
-
-            const headers = new HttpHeaders()
-                .set("Content-Type", "application/json");
-
-            let pathUrl: string = 'canvasGroups';
-            let finalUrl: string = this.setBaseUrl(pathUrl) + pathUrl;
-            this.http.post<CanvasHttpResponse>(finalUrl, data, {headers})
-            .subscribe(
-                res => {
-                    if(res.statusCode != 'success') {
-                        reject(res.message);
-                        return;
-                    };
-
-                    // Update Global vars to make sure they remain in sync
-                    this.canvasGroups.push(JSON.parse(JSON.stringify(res.data)));
-
-                    if (this.sessionDebugging) {
-                        console.log('addCanvasGroup ADDED', this.canvasGroups,
-                            this.canvasGroups)
-                    };
-
-                    resolve(res.data);
-                },
-                err => {
-                    if (this.sessionDebugging) {
-                        console.log('Error addCanvasGroup FAILED', {err});
-                    };
-
-                    reject(err.message);
-                }
-            )
-        });
-    }
-
     getWidgetGraphs(): Promise<WidgetGraph[]> {
         // Description: Gets all G
         // Returns: this.WidgetGraphs array, unless:
