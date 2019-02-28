e33fa462c8ed624bd0e87277ba81c5a0b52b4cd6
JannieI <jimmelman@gmail.com>
2019-02-28 13:57:31 +0200

    Landing getRes canvasBackgroundcolors

--- src/app/landing.component.ts
+++ src/app/landing.component.ts
@@ -201,7 +201,7 @@ export class LandingComponent implements OnInit {
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
 		// Get setup info
-		this.globalVariableService.getBackgroundColors();
+		this.globalVariableService.getResource('canvasBackgroundcolors');
 
 	}
 
