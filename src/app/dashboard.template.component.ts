8d91c2029843424843f825cd4664d86a7e1a0430
JannieI <jimmelman@gmail.com>
2019-03-03 20:01:41 +0200

    Dtmpl saveRes

--- src/app/dashboard.template.component.ts
+++ src/app/dashboard.template.component.ts
@@ -87,7 +87,7 @@ export class DashboardTemplateComponent implements OnInit {
         // Change Template ID and save
         this.currentTemplateName = '';
         this.currentDashboard.templateDashboardID = null;
-        this.globalVariableService.saveDashboard(this.currentDashboard);
+        this.globalVariableService.saveResource('dashboards', this.currentDashboard);
         this.globalVariableService.refreshCurrentDashboard(
             'DashboardTemplate clickClear',
             this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
@@ -112,7 +112,7 @@ export class DashboardTemplateComponent implements OnInit {
 
         // Change Template ID and save
         this.currentDashboard.templateDashboardID = dashboardID;
-        this.globalVariableService.saveDashboard(this.currentDashboard);
+        this.globalVariableService.saveResource('dashboards', this.currentDashboard);
         this.globalVariableService.refreshCurrentDashboard(
             'DashboardTemplate clickClear',
             this.globalVariableService.currentDashboardInfo.value.currentDashboardID,
