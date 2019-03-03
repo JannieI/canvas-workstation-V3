c1c6c2949bb9715dac82a737b4fad5e738d54f18
JannieI <jimmelman@gmail.com>
2019-03-03 14:36:55 +0200

    Dir ggle addRes

--- src/app/data.direct.googleSheets.component.ts
+++ src/app/data.direct.googleSheets.component.ts
@@ -332,7 +332,7 @@ export class DataDirectGoogleSheetsComponent implements OnInit {
             this.globalVariableService.addData(newData).then(resData => {
 
                 newdDataset.url = 'data/' + resData.id.toString();
-                this.globalVariableService.addDatasource(newDatasource).then(resDS => {
+                this.globalVariableService.addResource('datasources', newDatasource).then(resDS => {
                     newdDataset.datasourceID = resDS.id;
                     this.globalVariableService.addDataset(newdDataset);
 
