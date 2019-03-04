5d06101d084e61b7b419eb42813720dccfc1d3a2
JannieI <jimmelman@gmail.com>
2019-03-04 06:59:46 +0200

    Depr handleCloseDataManagedOverlayEditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1932,21 +1932,6 @@ export class AppComponent implements OnInit {
         };
     }
 
-    handleCloseDataManagedOverlayEditor(returnDS: Datasource) {
-        // Handle Close of Overlay Editor
-        this.globalFunctionService.printToConsole(this.constructor.name,'handleCloseDataManagedOverlayEditor', '@Start');
-
-        this.menuOptionClickPostAction();
-
-        this.showModalDataManagedOverlayEditor = false;
-
-        // Open Transformations if so requested
-        if (returnDS != null) {
-            this.selectedDatasource = returnDS;
-            this.showModalDataTransformation = true;
-        };
-    }
-
     handleCloseDataTransformation(action: string) {
         // Handle Close of Overlay Editor
         this.globalFunctionService.printToConsole(this.constructor.name,'handleCloseDataTransformation', '@Start');
