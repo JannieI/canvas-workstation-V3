b69f2441164b78c43c8d7b355767418b1de860da
JannieI <jimmelman@gmail.com>
2019-03-10 17:15:54 +0200

    App nav into form

---
+++ src/app/widget.navigator.component.ts
@@ -0,0 +1,56 @@
+/*
+ * Manage a single Graph component
+ */
+
+// From Angular
+import { Component }                  from '@angular/core';
+import { Input }                      from '@angular/core';
+
+// Our Services
+import { GlobalVariableService }      from './global-variable.service';
+import { GlobalFunctionService }      from './global-function.service';
+
+// Our Models
+import { Widget }                     from './models'
+
+
+@Component({
+    selector: 'widget-navigator',
+    templateUrl: './widget.navigator.component.html',
+    styleUrls: ['./widget.navigator.component.css']
+})
+export class WidgetNavigatorComponent {
+    @Input() widget: Widget;
+
+
+    constructor(
+        private globalFunctionService: GlobalFunctionService,
+        private globalVariableService: GlobalVariableService,
+
+    ) {
+        // Initialise
+        this.globalFunctionService.printToConsole(this.constructor.name,'constructor', '@Start');
+
+    }
+
+    ngOnInit() {
+        // Initialise
+        this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
+
+    }
+
+
+    clickSingleWidget() {
+        // Click W object
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickSingleWidget', '@Start');
+
+        // // TODO - fix index..
+        // this.currentWidgets[index].isSelected = !this.currentWidgets[index].isSelected;
+        // this.globalVariableService.currentWidgets.forEach(w => {
+        //     if (w.id == id) {
+        //         w.isSelected = this.currentWidgets[index].isSelected;
+        //     };
+        // });
+
+    }
+}
