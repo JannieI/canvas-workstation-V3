607dcb1d18338b8475529c4e251c92a72e1e268f
JannieI <jimmelman@gmail.com>
2019-03-10 18:43:49 +0200

    Nav, v2

--- src/app/widget.navigator.component.ts
+++ src/app/widget.navigator.component.ts
@@ -5,6 +5,11 @@
 // From Angular
 import { Component }                  from '@angular/core';
 import { Input }                      from '@angular/core';
+import { ElementRef }                 from '@angular/core';
+import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
+import { OnInit }                     from '@angular/core';
+import { ViewChild }                  from '@angular/core';
 
 // Our Services
 import { GlobalVariableService }      from './global-variable.service';
@@ -13,6 +18,11 @@ import { GlobalFunctionService }      from './global-function.service';
 // Our Models
 import { Widget }                     from './models'
 
+// Functions, 3rd Party
+import { compile }                    from 'vega-lite';
+import { parse }                      from 'vega';
+import { View }                       from 'vega';
+
 
 @Component({
     selector: 'widget-navigator',
@@ -20,8 +30,12 @@ import { Widget }                     from './models'
     styleUrls: ['./widget.navigator.component.css']
 })
 export class WidgetNavigatorComponent {
-    @Input() widget: Widget;
+    @ViewChild('dragWidget', {read: ElementRef}) dragWidget: ElementRef;  //Vega graph
+    @Input() selectedWidget: Widget;
 
+    showSpecificGraphLayer: boolean = false;
+    localWidget: Widget;                            // W to modify, copied from selected
+    specification: any;              // Full spec for Vega, or other grammar
 
     constructor(
         private globalFunctionService: GlobalFunctionService,
@@ -37,6 +51,25 @@ export class WidgetNavigatorComponent {
         // Initialise
         this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
 
+        // Deep copy Local W
+        this.localWidget = JSON.parse(JSON.stringify(this.selectedWidget));
+
+        // Create specification
+        this.specification = this.globalVariableService.createVegaSpec(
+            this.localWidget,
+            this.localWidget.graphHeight,
+            this.localWidget.graphWidth,
+            this.showSpecificGraphLayer,
+            0
+        );
+
+        // Render in DOM
+        let view = new View(parse(this.specification));
+        view.renderer('svg')
+            .initialize(this.dragWidget.nativeElement)
+            .hover()
+            .run()
+            .finalize();
     }
 
 
