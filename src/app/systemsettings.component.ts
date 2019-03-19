ca22f28cb7e4d6dbf10cbddaf0f399ca53ee23a7
JannieI <jimmelman@gmail.com>
2019-03-19 16:26:48 +0200

    Sett form - snapToGrid

--- src/app/systemsettings.component.ts
+++ src/app/systemsettings.component.ts
@@ -64,7 +64,6 @@ export class SystemSettingsComponent implements OnInit {
     printSize: string;
     printLayout: string;
     queryRunningMessage: string;
-    snapToGrid: boolean;
     widgetsMinZindex: number;
     widgetsMaxZindex: number;
     
@@ -84,7 +83,6 @@ export class SystemSettingsComponent implements OnInit {
         this.widgetsMinZindex = this.globalVariableService.canvasSettings.widgetsMinZindex;
         this.widgetsMaxZindex = this.globalVariableService.canvasSettings.widgetsMaxZindex;
         this.gridSize = this.globalVariableService.canvasSettings.gridSize;
-        // this.snapToGrid = this.globalVariableService.canvasSettings.snapToGrid;
         this.printDefault = this.globalVariableService.canvasSettings.printDefault;
         this.printSize = this.globalVariableService.canvasSettings.printSize;
         this.printLayout = this.globalVariableService.canvasSettings.printLayout;
@@ -124,7 +122,6 @@ export class SystemSettingsComponent implements OnInit {
             widgetsMinZindex: this.widgetsMinZindex,
             widgetsMaxZindex: this.widgetsMaxZindex,
             gridSize: this.gridSize,
-            // snapToGrid: this.snapToGrid,
             printDefault: this.printDefault,
             printSize: this.printSize,
             printLayout: this.printLayout,
