0552fe88d50a6e3c5cd1327cac62379ce959b826
JannieI <jimmelman@gmail.com>
2019-03-20 08:50:28 +0200

    DS Sch Log, v1

---
+++ src/app/data.datasourceScheduleLog.component.ts
@@ -0,0 +1,87 @@
+/*
+ * Shows all Schedules in the system
+ */
+
+// Angular
+import { Component }                  from '@angular/core';
+import { EventEmitter }               from '@angular/core';
+import { HostListener }               from '@angular/core';
+import { OnInit }                     from '@angular/core';
+import { Output }                     from '@angular/core';
+
+// Our Functions
+import { GlobalFunctionService } 	  from './global-function.service';
+
+// Our Services
+import { GlobalVariableService}       from './global-variable.service';
+
+// Models
+import { Datasource }                 from './models';
+import { DatasourceSchedule }         from './models';
+
+@Component({
+    selector: 'data-datasourceSchedule-log',
+    templateUrl: './data.datasourceScheduleLog.component.html',
+    styleUrls: ['./data.datasourceScheduleLog.component.css']
+})
+export class DataDatasourceScheduleLogComponent implements OnInit {
+
+    @Output() formDataDatasourceScheduleLogClosed: EventEmitter<string> = new EventEmitter();
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        console.log(event);
+        event.preventDefault();
+
+        // Known ones
+        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+            this.clickClose('Close');
+            return;
+        };
+
+    }
+
+    datasourceSchedules: DatasourceSchedule[];
+    datasources: Datasource[];
+    errorMessage: string = '';
+    selectedRow: number = 0;
+
+
+	constructor(
+        private globalFunctionService: GlobalFunctionService,
+        private globalVariableService: GlobalVariableService,
+	) {}
+
+    ngOnInit() {
+        // Initial
+        this.globalFunctionService.printToConsole(this.constructor.name,'ngOnInit', '@Start');
+
+        this.globalVariableService.getResource('datasources')
+            .then(data => this.datasources = data)
+            .catch(err => {
+                this.errorMessage = err.slice(0, 100);
+                console.error('Error in Datasource.schedules reading datasources: ' + err);
+            });
+        this.globalVariableService.getResource('datasourceSchedules')
+            .then(data => this.datasourceSchedules = data)
+            .catch(err => {
+                this.errorMessage = err.slice(0, 100);
+                console.error('Error in Datasource.schedules reading datasourceSchedules: ' + err);
+            });
+    }
+
+    clickClose(action: string) {
+        // Close form
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickClose', '@Start');
+
+        console.log('clickClose')
+
+		this.formDataDatasourceScheduleLogClosed.emit(action);
+    }
+  
+    clickRow(index: number) {
+        // Click Row
+        this.globalFunctionService.printToConsole(this.constructor.name,'clickRow', '@Start');
+        this.selectedRow = index;
+    }    
+}
