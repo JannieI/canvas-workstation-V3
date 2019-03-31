95c5090a14a2da30a55f008af18c9dd7ea6a0785
JannieI <jimmelman@gmail.com>
2019-03-31 16:37:17 +0200

    Add calcWfilter

--- src/app/table.single.component.ts
+++ src/app/table.single.component.ts
@@ -203,4 +203,15 @@ export class TableSingleComponent {
         }
     }
 
+    calcWfilter(columnName: string): string {
+        // Return * if given column in a Widget Filter
+        this.globalFunctionService.printToConsole(this.constructor.name,'calcWfilter', '@Start');
+
+        if (this.widgetFilterColumns.indexOf(columnName) >= 0) {
+            return '*';
+        } else {
+            return '';
+        };
+    }
+
 }
