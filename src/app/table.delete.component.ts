7125e4ea399c4906684559971c1abaf3315d125b
JannieI <jimmelman@gmail.com>
2019-04-09 08:23:18 +0200

    Tbl Del ===

--- src/app/table.delete.component.ts
+++ src/app/table.delete.component.ts
@@ -40,12 +40,12 @@ export class TableDeleteComponent implements OnInit {
         event.preventDefault();
 
         // Known ones
-        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+        if (event.code === 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
             this.clickClose();
             return;
         };
         if (
-            (event.code == 'Enter'  ||  event.code == 'NumpadEnter')
+            (event.code === 'Enter'  ||  event.code === 'NumpadEnter')
             &&
             (!event.ctrlKey)
             &&
