653de3a857cb683da483823e771764226af81542
JannieI <jimmelman@gmail.com>
2019-04-09 08:56:03 +0200

    SysMsg ===

--- src/app/collaborate.systemMessages.component.ts
+++ src/app/collaborate.systemMessages.component.ts
@@ -33,7 +33,7 @@ export class CollaborateSystemMessagesComponent implements OnInit {
         event.preventDefault();
 
         // Known ones
-        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+        if (event.code === 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
             this.clickClose('Close');
             return;
         };
