df2671f5faf65c18bf3ce37e153b92abc3b31805
JannieI <jimmelman@gmail.com>
2019-04-09 08:54:03 +0200

    Dsav ===

--- src/app/dashboard.save.component.ts
+++ src/app/dashboard.save.component.ts
@@ -31,12 +31,12 @@ export class DashboardSaveComponent implements OnInit {
         event.preventDefault();
 
         // Known ones
-        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+        if (event.code === 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
             this.clickClose('Close');
             return;
         };
         if (
-            (event.code == 'Enter'  ||  event.code == 'NumpadEnter')
+            (event.code === 'Enter'  ||  event.code === 'NumpadEnter')
             &&
             (!event.ctrlKey)
             && 
