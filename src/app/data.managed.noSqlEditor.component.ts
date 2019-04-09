e28371b82a6a48b12917f5245f2cbc24c1041d80
JannieI <jimmelman@gmail.com>
2019-04-09 08:35:59 +0200

    Man NoSQL ===

--- src/app/data.managed.noSqlEditor.component.ts
+++ src/app/data.managed.noSqlEditor.component.ts
@@ -38,7 +38,7 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
         event.preventDefault();
 
         // Known ones
-        if (event.code == 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
+        if (event.code === 'Escape'  &&  (!event.ctrlKey)  &&  (!event.shiftKey)  ) {
             this.clickClose('Close');
             return;
         };
@@ -83,7 +83,7 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
                 console.error('Error in managed.noSQL reading dataConnections: ' + err);
             });
 
-        if (this.selectedDatasource == null) {
+        if (this.selectedDatasource === null) {
             let today: Date = new Date();
             this.selectedDatasource = {
                 id: null,
@@ -179,7 +179,7 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
 
         // Get connection detail
         let connection: DataConnection[] = this.dataConnections.filter(
-            con => con.connectionName == this.connectionName
+            con => con.connectionName === this.connectionName
         );
         let serverType: string = '';
         let serverName: string = '';
@@ -194,7 +194,7 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
 
         // Get the driver
         let driver: string = this.serverTypes
-            .filter(styp => styp.serverType == serverType)
+            .filter(styp => styp.serverType === serverType)
             .map(styp => styp.driverName)[0];
 
         // Build Spec
@@ -264,11 +264,11 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
         this.globalFunctionService.printToConsole(this.constructor.name,'clickSave', '@Start');
 
         // Validation
-        if (this.selectedDatasource.name == ''  ||  this.selectedDatasource.name == null) {
+        if (this.selectedDatasource.name === ''  ||  this.selectedDatasource.name === null) {
             this.errorMessage = 'The name is compulsory';
             return;
         };
-        if (this.selectedDatasource.description == ''  ||  this.selectedDatasource.description == null) {
+        if (this.selectedDatasource.description === ''  ||  this.selectedDatasource.description === null) {
             this.errorMessage = 'The description is compulsory';
             return;
         };
@@ -323,7 +323,7 @@ export class DataManagedNoSQLEditorComponent implements OnInit {
         };
 
         // Close form and open Transitions if requested
-        if (action == 'Saved') {
+        if (action === 'Saved') {
             this.formDataManagedNoSQLEditorClosed.emit(null);
 
         } else {
