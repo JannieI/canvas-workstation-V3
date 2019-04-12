401f04aca2d2fefeb076b78c04afa73940d6fc66
JannieI <jimmelman@gmail.com>
2019-04-12 13:04:21 +0200

    navNodesFilteredPerProperty

--- src/app/widget.navigator.component.ts
+++ src/app/widget.navigator.component.ts
@@ -1203,14 +1203,18 @@ console.log('xx this.specification', this.graphTitle, this.graphData, this.speci
 
 
         // Find Top 40 'Companies'
-        for (var i = 0; i < this.networkGraph[1].length; i++) {
-            if (this.networkGraph[2][i] == 'Company'  &&  this.networkGraph[3][i] == 'Top 40') {
-                console.log('xx Company Prop in Row 1, col i', i)
-                break;
-            };
-        };
-        let parentNodesTop40: string[] = this.networkGraph.filter(row => row[i] == '1').map(x => x[0])
-        console.log('xx Top 40 Companies:', parentNodesTop40) 
+        // for (var i = 0; i < this.networkGraph[1].length; i++) {
+        //     if (this.networkGraph[2][i] == 'Company'  &&  this.networkGraph[3][i] == 'Top 40') {
+        //         console.log('xx Company Prop in Row 1, col i', i)
+        //         break;
+        //     };
+        // };
+        // let parentNodesTop40: string[] = this.networkGraph.filter(row => row[i] == '1').map(x => x[0])
+        // console.log('xx Top 40 Companies:', parentNodesTop40) 
+        let nodeProperty: string[] = this.navPropertiesPerNodeType(nodeTypes[0]);
+        console.log('xx node filtered on :', nodeTypes[0], nodeProperty[0], 
+            this.navNodesFilteredPerProperty(nodeTypes[0], nodeProperty[0]))
+
 
 
         // Children for C
