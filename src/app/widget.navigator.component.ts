ec4075bbd9f3eeaac762bafa9b5c3ddd51512c71
JannieI <jimmelman@gmail.com>
2019-04-23 17:26:38 +0200

    Depr filterChildValue

--- src/app/widget.navigator.component.ts
+++ src/app/widget.navigator.component.ts
@@ -117,7 +117,6 @@ export class WidgetNavigatorComponent {
     childNodeFilter: NavigatorNodeFiler[] = [];         // Actual Filter
     childFilterErrorMessage: string = '';
     filteredChildNodes: string[] = [];                  // List of Node, after filtered on NodeProperties
-    filterChildValue: string = '';
     filterID: number = -1;
     firstAdjacencyCellRowNr: number = -1;
     parentFilterErrorMessage: string = '';
