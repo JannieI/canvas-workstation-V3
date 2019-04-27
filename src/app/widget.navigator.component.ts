41e504c202ea77504c6ea403db3c9570bab23f7e
JannieI <jimmelman@gmail.com>
2019-04-27 10:31:20 +0200

    createGraphCommonNodeView

--- src/app/widget.navigator.component.ts
+++ src/app/widget.navigator.component.ts
@@ -794,6 +794,34 @@ export class WidgetNavigatorComponent {
     createGraphCommonNodeView(inputHeight: number = 0, inputWidth: number = 0, addToHistory: boolean = true) {
         // Create the data for the view
         this.globalFunctionService.printToConsole(this.constructor.name, 'createGraphCommonNodeView', '@Start');
+        
+        // Reset
+        this.graphData = [];
+
+        console.log('xx this.childDataAll', this.childDataAll)
+
+        if (this.childDataAll.length > 0) {
+
+            this.graphData = this.constructGraphDataForCommonNode(
+                'Mr van Wyk, Rene', this.childDataAll);
+
+            // TODO
+            // 1. add to History
+            // 2. Set H & W
+            if (inputWidth != 0) {
+                this.graphWidth = inputWidth;
+            } else {
+                if (this.localWidget.graphLayers.length > 0) {
+                    this.graphWidth = this.localWidget.graphLayers[0].graphSpecification.width;
+                };
+            };
+            if (this.graphWidth < 100) {
+                this.graphWidth = 100;
+            };
+
+        };
+        console.log('xx this.graphData', this.graphData)
+
     }
 
     createGraphDistanceView(inputHeight: number = 0, inputWidth: number = 0, addToHistory: boolean = true) {
