9c8258be1b846612321417ce3857e41bfc98530e
JannieI <jimmelman@gmail.com>
2019-04-28 09:50:21 +0200

    checkShowGraph

--- src/app/widget.navigator.component.ts
+++ src/app/widget.navigator.component.ts
@@ -634,7 +634,10 @@ export class WidgetNavigatorComponent {
                         this.createGraphDistanceView();
                         break;
                     }
-                    CustomView
+                    case 'CustomView': {
+                        this.createGraphCustomView();
+                        break;
+                    }
                     case 'NodeTypeView': {
                         this.createGraphNodeTypeView();
                         break;
@@ -903,7 +906,7 @@ export class WidgetNavigatorComponent {
     createGraphDistanceView(inputHeight: number = 0, inputWidth: number = 0, addToHistory: boolean = true) {
         // Create the data for the view
         this.globalFunctionService.printToConsole(this.constructor.name, 'createGraphDistanceView', '@Start');
-        // this.nav2WalkInPath(null, "y", "rel", 0, [])
+
         this.nav2WalkInPath(null, 'Mr van Wyk, Rene', "Director Of", 0, [])
         console.log('xx @END of createGraphDistanceView', this.routesPerNode)
 
