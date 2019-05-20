0512a65a2551850349c3e9b509b5dd2c7771d357
JannieI <jimmelman@gmail.com>
2019-05-20 11:30:53 +0200

    deleteDatasource Token

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -559,7 +559,9 @@ export class GlobalVariableService {
         return new Promise<any>((resolve, reject) => {
 
             const headers = new HttpHeaders()
-                .set("Content-Type", "application/json");
+                .set("Content-Type", "application/json")
+                .set("Accept", "application/json")
+                .set("Authorization", "JWT " + this.currentToken);
 
             let pathUrl: string = '/canvasDatasource';
             let finalUrl: string = this.canvasServerURI + pathUrl;
