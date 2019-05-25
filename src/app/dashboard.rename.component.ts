915cf04a0e6e32653f35de2928521e0c04d4f5e3
JannieI <jimmelman@gmail.com>
2019-05-25 20:41:50 +0200

    D ren Err Msg

--- src/app/dashboard.rename.component.ts
+++ src/app/dashboard.rename.component.ts
@@ -97,7 +97,7 @@ export class DashboardRenameComponent implements OnInit {
             })
             .catch(err => {
                 console.error('Error in Dashboard.rename reading dashboards: ' + err)
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
             });
 }
 
