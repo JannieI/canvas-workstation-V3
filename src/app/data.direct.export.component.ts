8207b33a0ed81fe7223bdd8bde0d4e59963941fb
JannieI <jimmelman@gmail.com>
2019-05-25 20:48:21 +0200

    Dat Exprt Err Msg

--- src/app/data.direct.export.component.ts
+++ src/app/data.direct.export.component.ts
@@ -61,7 +61,7 @@ export class DataDirectExportComponent implements OnInit {
             this.datasources = res;
         })
         .catch(err => {
-            this.errorMessage = err.slice(0, 100);
+            this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
             console.error('Error in Datasource.export reading datasources: ' + err);
         });        
     }
