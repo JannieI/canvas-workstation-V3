84f147d6d5ddfb6184615006a53329932d213c86
JannieI <jimmelman@gmail.com>
2019-05-25 20:45:37 +0200

    Comb Err Msg

--- src/app/data.combination.component.ts
+++ src/app/data.combination.component.ts
@@ -90,7 +90,7 @@ export class DataCombinationComponent implements OnInit {
             };
         })
         .catch(err => {
-            this.errorMessage = err.slice(0, 100);
+            this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
             console.error('Error in Data.combination reading datasources: ' + err);
         });
 
