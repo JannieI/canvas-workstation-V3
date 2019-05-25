dfc861008f411a04e091583ab0947cd8ccaeb066
JannieI <jimmelman@gmail.com>
2019-05-25 20:45:09 +0200

    bGloss Err Msg

--- src/app/data.businessGlossary.component.ts
+++ src/app/data.businessGlossary.component.ts
@@ -68,7 +68,7 @@ export class DataBusinessGlossaryComponent implements OnInit {
                 };
             })
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in data.businessGlossary reading datasources: ' + err);
             });
 
