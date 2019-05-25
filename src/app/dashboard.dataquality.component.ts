d8fc292d9bd26982755d4f9f8db3a1e3afceb300
JannieI <jimmelman@gmail.com>
2019-05-25 20:35:08 +0200

    D qual Err Msg

--- src/app/dashboard.dataquality.component.ts
+++ src/app/dashboard.dataquality.component.ts
@@ -83,7 +83,7 @@ export class DashboardDataQualityComponent implements OnInit {
                     dsArray.indexOf(c['datasourceID']) >= 0)
             })
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in Dashboard.dataQuality reading dataQualityIssues: ' + err);
             });
     }
