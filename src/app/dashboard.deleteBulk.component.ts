116e37b266382a2f306df2da145a52fb33107fde
JannieI <jimmelman@gmail.com>
2019-05-25 20:40:08 +0200

    Bulk Err Msg

--- src/app/dashboard.deleteBulk.component.ts
+++ src/app/dashboard.deleteBulk.component.ts
@@ -90,7 +90,7 @@ export class DashboardDeleteBulkComponent implements OnInit {
             })
             .catch(err => {
                 console.error('Error in Dashboard.bulkDelete reading dashboards: ' + err)
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
             });
 }
 
