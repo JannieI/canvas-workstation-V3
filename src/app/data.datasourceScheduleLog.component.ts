87ee90f082b5b37219737ed0aef954248460d2fc
JannieI <jimmelman@gmail.com>
2019-05-25 20:47:14 +0200

    DS sch Log Err Msg

--- src/app/data.datasourceScheduleLog.component.ts
+++ src/app/data.datasourceScheduleLog.component.ts
@@ -58,7 +58,7 @@ export class DataDatasourceScheduleLogComponent implements OnInit {
         this.globalVariableService.getResource('datasourceScheduleLogs')
             .then(data => this.datasourceScheduleLogs = data)
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in Datasource.schedules reading datasourceScheduleLogs: ' + err);
             });
     }
