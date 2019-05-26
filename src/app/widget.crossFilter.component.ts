344e86a6f5c0d4c1643ea2a15256833739390870
JannieI <jimmelman@gmail.com>
2019-05-26 09:35:41 +0200

    W cross Err Msg

--- src/app/widget.crossFilter.component.ts
+++ src/app/widget.crossFilter.component.ts
@@ -237,7 +237,7 @@ export class WidgetCrossFilterComponent implements OnInit {
 
                 })
                 .catch(err => {
-                    this.errorMessage = err.slice(0, 100);
+                    this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                     console.error('Error in widget.crossFilter saving Target Widget: ' + err);
                 });
         };
@@ -297,7 +297,7 @@ export class WidgetCrossFilterComponent implements OnInit {
                 );
             } )
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in widget.crossFilter saving Widget: ' + err);
             });
         };
