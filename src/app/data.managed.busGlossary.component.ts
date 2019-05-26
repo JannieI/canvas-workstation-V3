8afd3556f26db77725741c1c45e80cf98da33e5b
JannieI <jimmelman@gmail.com>
2019-05-26 09:29:02 +0200

    Man Gloss Err Msg

--- src/app/data.managed.busGlossary.component.ts
+++ src/app/data.managed.busGlossary.component.ts
@@ -73,7 +73,7 @@ export class DataManagedBusGlossaryComponent implements OnInit {
             };
         })
         .catch(err => {
-            this.errorMessage = err.slice(0, 100);
+            this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
             console.error('Error in Managed.businessGlossary reading auditTrails: ' + err);
         });
 
@@ -149,7 +149,7 @@ export class DataManagedBusGlossaryComponent implements OnInit {
             };
             this.globalVariableService.saveResource('datasources', this.selectedDatasource)
                 .catch(err => {
-                    this.errorMessage = err.slice(0, 100);
+                    this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                     console.error('Error in Managed.businessGlossary reading auditTrails: ' + err);
                 });
 
