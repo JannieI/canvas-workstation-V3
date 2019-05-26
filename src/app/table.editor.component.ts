90f622d585f90e32d04cae03679d0458856808d1
JannieI <jimmelman@gmail.com>
2019-05-26 09:34:07 +0200

    Tbl Ed Err Msg

--- src/app/table.editor.component.ts
+++ src/app/table.editor.component.ts
@@ -220,7 +220,7 @@ import { GlobalVariableService }      from './global-variable.service';
 
                 })
                 .catch(err => {
-                    this.errorMessage = err.slice(0, 100);
+                    this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                     console.error('Error in table.editor reading widgetGraphs: ' + err);
                 });
     
@@ -331,7 +331,7 @@ import { GlobalVariableService }      from './global-variable.service';
 
                 })
                 .catch(err => {
-                    this.errorMessage = err.slice(0, 100);
+                    this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                     console.error('Error in table.editor adding widgets: ' + err);
                 });
 
@@ -374,7 +374,7 @@ import { GlobalVariableService }      from './global-variable.service';
 
                 })
                 .catch(err => {
-                    this.errorMessage = err.slice(0, 100);
+                    this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                     console.error('Error in table.editor saveWidget: ' + err);
                 });
 
