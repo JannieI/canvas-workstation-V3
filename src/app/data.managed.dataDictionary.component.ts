c8252ac684db2036c766e0442c5d0e75e63fdb7a
JannieI <jimmelman@gmail.com>
2019-05-26 09:29:27 +0200

    Dict Err Msg

--- src/app/data.managed.dataDictionary.component.ts
+++ src/app/data.managed.dataDictionary.component.ts
@@ -75,7 +75,7 @@ export class DataManagedDataDictionaryComponent implements OnInit {
                 };
             })
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in Managed.dictionary reading datasources: ' + err);
             });
 
@@ -150,7 +150,7 @@ export class DataManagedDataDictionaryComponent implements OnInit {
             };
             this.globalVariableService.saveResource('datasources', this.selectedDatasource)
                 .catch(err => {
-                    this.errorMessage = err.slice(0, 100);
+                    this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                     console.error('Error in Managed.dictionary reading datasources: ' + err);
                 });
 
