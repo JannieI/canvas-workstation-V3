d30c625f0a2b97779be844b4027df881267cc201
JannieI <jimmelman@gmail.com>
2019-05-26 09:37:31 +0200

    Wcont Err Msg

--- src/app/widget.container.component.ts
+++ src/app/widget.container.component.ts
@@ -190,7 +190,7 @@ export class WidgetContainerComponent implements OnInit {
 
             })
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in widget.container reading canvasBackgroundcolors: ' + err);
             });
 
