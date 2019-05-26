a4920558bad8096f893f77b2513868437b898fc8
JannieI <jimmelman@gmail.com>
2019-05-26 09:35:56 +0200

    Wdel Err Msg

--- src/app/widget.delete.component.ts
+++ src/app/widget.delete.component.ts
@@ -90,7 +90,7 @@ export class WidgetDeleteComponent implements OnInit {
                 };
             })
             .catch(err => {
-                this.errorMessage = err.slice(0, 100);
+                this.errorMessage = !err.message?  err.slice(0, 100)  :  err.message.slice(0, 100);
                 console.error('Error in widget.delete reading widgetStoredTemplates: ' + err);
             });
 
