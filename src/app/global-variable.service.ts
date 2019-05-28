3b191fab0bc1ac7fe764bc5f1cb0d9f8cc0703d3
JannieI <jimmelman@gmail.com>
2019-05-28 19:36:25 +0200

    getExecQuery

--- src/app/global-variable.service.ts
+++ src/app/global-variable.service.ts
@@ -6219,9 +6219,17 @@ console.log('xx Ivan lss', lss)
                 .set("Authorization", "Bearer " + this.currentToken);
 
             this.http.get<CanvasHttpResponse>(finalUrl, {headers}).subscribe(
-                res  => {
-                    if(res.statusCode != 'success') {
-                        reject(res.message);
+                httpResponse  => {
+                    if(httpResponse.statusCode != 'success') {
+                        reject(httpResponse.message);
+                        return;
+                    };
+                    if(httpResponse.statusCode != 'success') {
+                        reject(httpResponse.message);
+                        return;
+                    };
+                    if(httpResponse.data == null) {
+                        reject('Data in response object is null; it should be an array');
                         return;
                     };
 
@@ -6229,9 +6237,9 @@ console.log('xx Ivan lss', lss)
                     if (this.sessionDebugging) {
                         console.log('%c    Global-Variables getExecQuery ends',
                             this.concoleLogStyleForEndOfMethod,
-                            res.data.length);
+                            httpResponse.data.length);
                     };
-                    resolve(res);
+                    resolve(httpResponse);
                 },
                 err => {
                     reject(err.message  || err.sqlMessage);
